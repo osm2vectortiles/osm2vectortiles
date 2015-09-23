ce93e968a6fe092b100da17e204f6843d3882c39
Lukas Martinelli <me@lukasmartinelli.ch>
2015-09-23 14:16:55 +0200

    Make sure tileserver runs smoothly with kitematic

--- tileserver/README.md
+++ tileserver/README.md
@@ -3,42 +3,51 @@
 Render raster data from vector tiles and style projects
 on the fly with the help of [tessera](https://github.com/mojodna/tessera).
 
-You plug in your vector tiles and style projects and
-you have a hosted tileserver.
+You plug in your own vector tiles and style projects and get
+a raster tile server.
 
 ## Kitematic Usage
 
-## Docker Usage
+Start a new container by searching for `lukasmartinelli/tileserver`
+(in future `geometalab/tileserver`).
+The container will complain about missing `tm2` style projects.
+
+![Container started unsucessfully](screenshots/tileserver_kitematic_started.png)
 
 Mount your `mbtiles` files and `tm2` style projects into the `/data` volume.
-If your `mbtiles` sources are named the same as your `tm2` project the
-script will automatically replace the source specified in the `data.yml`
-of your `tm2` project with the according `mbtiles` source.
 
-Original source URI.
+![Configured volumes for container](screenshots/tileserver_kitematic_volumes.png)
+
+Now restart the container. You should be up and running serving generated raster tiles.
+
+![Container running and serving tiles](screenshots/tileserver_kitematic_running.png)
+
+## Docker Usage
+
+Assuming you are in the folder containing your `mbtiles` files and `tm2` style projects.
 
 ```
-mapbox:///klokantech.97cbd1e1
+docker run -p 80:80 -v $(pwd):/data osm2vectortiles/tileserver
 ```
 
-Will be changed to the `mbtiles` source of the same name.
+Visit `localhost:80` to see a [leaflet](http://leafletjs.com/)
+map of the rendered raster tiles.
 
-```
-mbtiles:///data/countries.mbtiles
-```
+### Vector Tiles Source Configuration
 
-Give the current folder contains the following files and directories.
+The tileserver is meant to server the tiles from your own
+vector tiles. You can develop your stylesheet with a local `tm2source` source
+or a `mapbox` source but once you serve the tiles you need to reference a `mbtiles` source.
+
+If you have a `mbtiles` file named the same as your `tm2` project in the data directory,
+the server will automatically replace your previous source with a new source.
+
+Given the current folder contains the following files and directories.
 
 ```bash
 ├── countries.tm2
 └── countries.mbtiles
 ```
 
-You can run a docker container.
-
-```
-docker run -p 8080:8080 -v $(pwd):/data osm2vectortiles/tileserver
-```
-
-Visit `localhost:8080` to see a [leaflet](http://leafletjs.com/)
-map of the rendered raster tiles.
+The original source `mapbox:///klokantech.97cbd1e1` in the `project.yml` will
+now be replaced with `mbtiles:///data/countries.mbtiles`.
--- tileserver/run.sh
+++ tileserver/run.sh
@@ -9,34 +9,48 @@ port=${PORT:-8080}
 cache_size=${CACHE_SIZE:-10}
 source_cache_size=${SOURCE_CACHE_SIZE:-10}
 
+
+
+# find first tm2 project
+if [ "$(ls -A $source_data_dir)" ]; then
+    for _project_dir in "$source_data_dir"/*.tm2; do
+        [ -d "${_project_dir}" ] && project_dir="${_project_dir}" && break
+    done
+else
+    echo "No tm2 projects found. Please mount the $source_data_dir volume to a folder containing tm2 projects."
+    exit 404
+fi
+
 # find all tm2 projects but only the first project will be hosted for now
-for project_dir in "$source_data_dir"/*.tm2; do
-    project_name="${project_dir##*/}"
-    project_config_file="${project_dir%%/}/project.yml"
-    vectortiles_name="${project_name%.tm2}.mbtiles"
-    mbtiles_file="${source_data_dir%%/}/$vectortiles_name"
+project_name="${project_dir##*/}"
+project_config_file="${project_dir%%/}/project.yml"
+vectortiles_name="${project_name%.tm2}.mbtiles"
+mbtiles_file="${source_data_dir%%/}/$vectortiles_name"
 
-    # project config will be copied to new folder because we
-    # modify the source configuration of the style and don't want
-    # that to effect the original file
-    dest_project_dir="${dest_data_dir%%/}/$project_name"
-    dest_project_config_file="${dest_project_dir%%/}/project.yml"
-    cp -r "$project_dir" "$dest_project_dir"
+echo "Found project ${project_name}.tm2"
 
-    # project.yml is single source of truth, therefore the mapnik
-    # stylesheet is not necessary
-    rm "${dest_project_dir%%/}/project.xml"
+# project config will be copied to new folder because we
+# modify the source configuration of the style and don't want
+# that to effect the original file
+dest_project_dir="${dest_data_dir%%/}/$project_name"
+dest_project_config_file="${dest_project_dir%%/}/project.yml"
+cp -rf "$project_dir" "$dest_project_dir"
 
-    # replace external vector tile sources with mbtiles source
-    # this allows developing rapidyl with an external source and then use the
-    # mbtiles for dependency free deployment
-    if [ -f "$mbtiles_file" ]; then
-        replace_expr="s|source: \".*\"|source: \"mbtiles://$mbtiles_file\"|g"
-        sed -e "$replace_expr" $project_config_file > $dest_project_config_file
-    fi
+# project.yml is single source of truth, therefore the mapnik
+# stylesheet is not necessary
+rm -f "${dest_project_dir%%/}/project.xml"
 
-    exec tessera "tmstyle://$dest_project_dir" \
-        --port $port \
-        --cache-size $cache_size \
-        --source-cache-size $source_cache_size
-done
+# replace external vector tile sources with mbtiles source
+# this allows developing rapidyl with an external source and then use the
+# mbtiles for dependency free deployment
+if [ -f "$mbtiles_file" ]; then
+    replace_expr="s|source: \".*\"|source: \"mbtiles://$mbtiles_file\"|g"
+    sed -e "$replace_expr" $project_config_file > $dest_project_config_file
+fi
+
+echo "Associating $vectortiles_name with $project_name"
+
+exec tessera "tmstyle://$dest_project_dir" \
+    --port $port \
+    --cache-size $cache_size \
+    --source-cache-size $source_cache_size
