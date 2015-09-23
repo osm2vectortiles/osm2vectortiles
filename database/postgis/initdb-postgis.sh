22fe830593c98c6bfdfc8e05ffa60ff7c299392b
Lukas Martinelli <me@lukasmartinelli.ch>
2015-09-23 19:49:56 +0200

    Fix postgis script

--- database/postgis/initdb-postgis.sh
+++ database/postgis/initdb-postgis.sh
@@ -13,12 +13,12 @@ EOSQL
 # Load PostGIS into both template_database and $POSTGRES_DB
 cd "/usr/share/postgresql/$PG_MAJOR/contrib/postgis-$POSTGIS_MAJOR"
 for DB in template_postgis "$POSTGRES_DB"; do
-    echo "Loading PostGIS into $DB via CREATE EXTENSION"
-    psql --dbname="$DB" <<-'EOSQL'
-        CREATE EXTENSION postgis;
-        CREATE EXTENSION postgis_topology;
-        CREATE EXTENSION fuzzystrmatch;
-        CREATE EXTENSION postgis_tiger_geocoder;
-        CREATE EXTENSION hstore;
-    EOSQL
+	echo "Loading PostGIS into $DB via CREATE EXTENSION"
+	psql --dbname="$DB" <<-'EOSQL'
+		CREATE EXTENSION postgis;
+		CREATE EXTENSION hstore;
+		CREATE EXTENSION postgis_topology;
+		CREATE EXTENSION fuzzystrmatch;
+		CREATE EXTENSION postgis_tiger_geocoder;
+	EOSQL
 done
