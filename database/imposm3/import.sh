0c4f71018fbf37ad264d250f0d320d33ba9ac797
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-12 18:06:04 +0200

    More defensive bash script for import

--- database/imposm3/import.sh
+++ database/imposm3/import.sh
@@ -3,30 +3,46 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-IMPORT_DATA_DIR=${IMPORT_DATA_DIR:-/data/import}
-IMPOSM_CACHE_DIR=${IMPOSM_CACHE_DIR:-/data/cache}
+readonly IMPORT_DATA_DIR=${IMPORT_DATA_DIR:-/data/import}
+readonly IMPOSM_CACHE_DIR=${IMPOSM_CACHE_DIR:-/data/cache}
 
-IMPOSM_BIN=${IMPOSM_BIN:-/imposm3}
-MAPPING_JSON=${MAPPING_JSON:-/usr/src/app/mapping.json}
+readonly IMPOSM_BIN=${IMPOSM_BIN:-/imposm3}
+readonly MAPPING_JSON=${MAPPING_JSON:-/usr/src/app/mapping.json}
 
-OSM_DB=${OSM_DB:-osm}
-OSM_USER=${OSM_USER:-osm}
-OSM_PASSWORD=${OSM_PASSWORD:-osm}
+readonly OSM_DB=${OSM_DB:-osm}
+readonly OSM_USER=${OSM_USER:-osm}
+readonly OSM_PASSWORD=${OSM_PASSWORD:-osm}
 
-DB_SCHEMA=public
-PG_CONNECT="postgis://$OSM_USER:$OSM_PASSWORD@$DB_PORT_5432_TCP_ADDR/$OSM_DB"
+readonly DB_SCHEMA=${OSM_SCHEMA:-public}
+readonly DB_HOST=$DB_PORT_5432_TCP_ADDR
+readonly PG_CONNECT="postgis://$OSM_USER:$OSM_PASSWORD@$DB_HOST/$OSM_DB"
 
-if [ "$(ls -A $IMPORT_DATA_DIR/*.pbf 2> /dev/null)" ]; then
-    for PBF_FILE in "$IMPORT_DATA_DIR"/*.pbf; do
-        $IMPOSM_BIN import -connection $PG_CONNECT -mapping $MAPPING_JSON -appendcache -cachedir=$IMPOSM_CACHE_DIR -read $PBF_FILE
-        $IMPOSM_BIN import -connection $PG_CONNECT -mapping $MAPPING_JSON -appendcache -cachedir=$IMPOSM_CACHE_DIR -write -diff -dbschema-import=${DB_SCHEMA}
-    done
-else
-    echo "No PBF files for import found."
-    echo "Please mount the $IMPORT_DATA_DIR volume to a folder containing OSM PBF files."
-    exit 404
-fi
+function read_pbf_into_cache() {
+    local pbf_file=$1
+    $IMPOSM_BIN import -connection $PG_CONNECT -mapping $MAPPING_JSON -appendcache -cachedir=$IMPOSM_CACHE_DIR -read $pbf_file
+}
 
-if ! [ "$(ls -A $IMPORT_DATA_DIR)" ]; then
-    echo "To speed up imposm import over subsequent runs you should mount the $IMPOSM_CACHE_DIR to a empty folder."
-fi
+function import_from_cache() {
+    $IMPOSM_BIN import -connection $PG_CONNECT -mapping $MAPPING_JSON -appendcache -cachedir=$IMPOSM_CACHE_DIR -write -diff -dbschema-import=${DB_SCHEMA}
+}
+
+function main() {
+    if [ "$(ls -A $IMPORT_DATA_DIR/*.pbf 2> /dev/null)" ]; then
+        local pbf_file
+        for pbf_file in "$IMPORT_DATA_DIR"/*.pbf; do
+            read_pbf_into_cache $pbf_file
+            import_from_cache
+        done
+    else
+        echo "No PBF files for import found."
+        echo "Please mount the $IMPORT_DATA_DIR volume to a folder containing OSM PBF files."
+        exit 404
+    fi
+
+    if ! [ "$(ls -A $IMPORT_DATA_DIR)" ]; then
+        echo "To speed up imposm import over subsequent runs you should mount the $IMPOSM_CACHE_DIR to a empty folder."
+    fi
+}
+
+main
+
