d4295cabafcfaa9d8a6fa1a89f72242c768757aa
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-12 18:52:13 +0200

    Refactor PostgreSQL bash scripts

--- database/postgis/initdb-postgis.sh
+++ database/postgis/initdb-postgis.sh
@@ -3,27 +3,37 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# perform all actions as $POSTGRES_USER
-export PGUSER="$POSTGRES_USER"
-
-# create the 'template_postgis' template db
-psql --dbname="$POSTGRES_DB" <<- 'EOSQL'
-CREATE DATABASE template_postgis;
-UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
-EOSQL
-
-# load PostGIS into both template_database and $POSTGRES_DB
-cd "/usr/share/postgresql/$PG_MAJOR/contrib/postgis-$POSTGIS_MAJOR"
-for DB in template_postgis "$POSTGRES_DB"; do
-	echo "Loading PostGIS into $DB"
-	psql --dbname="$DB" <<-'EOSQL'
-		CREATE EXTENSION postgis;
-		CREATE EXTENSION hstore;
-		CREATE EXTENSION postgis_topology;
-		CREATE EXTENSION fuzzystrmatch;
-		CREATE EXTENSION postgis_tiger_geocoder;
+function create_template_postgis() {
+    PGUSER="$POSTGRES_USER" psql --dbname="$POSTGRES_DB" <<-'EOSQL'
+		CREATE DATABASE template_postgis;
+		UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
 	EOSQL
-done
+}
 
-echo "Loading vt-util functions into template_postgis"
-psql --dbname="template_postgis" -f "$VT_UTIL_DIR/postgis-vt-util.sql"
+function install_vt_util() {
+    echo "Loading vt-util functions into template_postgis"
+    PGUSER="$POSTGRES_USER" psql --dbname="template_postgis" -f "$VT_UTIL_DIR/postgis-vt-util.sql"
+}
+
+function create_postgis_extensions() {
+    cd "/usr/share/postgresql/$PG_MAJOR/contrib/postgis-$POSTGIS_MAJOR"
+    local db
+    for db in template_postgis "$POSTGRES_DB"; do
+        echo "Loading PostGIS into $db"
+        PGUSER="$POSTGRES_USER" psql --dbname="$db" <<-'EOSQL'
+			CREATE EXTENSION postgis;
+			CREATE EXTENSION hstore;
+			CREATE EXTENSION postgis_topology;
+			CREATE EXTENSION fuzzystrmatch;
+			CREATE EXTENSION postgis_tiger_geocoder;
+		EOSQL
+    done
+    }
+
+function main() {
+    create_template_postgis
+    create_postgis_extensions
+    install_vt_util
+}
+
+main
