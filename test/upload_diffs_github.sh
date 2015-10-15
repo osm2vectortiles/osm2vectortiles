8df0991f7e24fe61761fab3543956cf0b75850d1
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-15 20:16:28 +0200

    Try upload diffs onto Github

---
+++ test/upload_diffs_github.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+readonly CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly DIFF_DIR="$CWD/local"
+
+readonly GIT_USERNAME="Travis CI"
+readonly GIT_EMAIL="me@lukasmartinelli.ch"
+
+function commit_latest_diffs() {
+    git clone https://github.com/geometalab/osm2vectortiles.git repo
+
+    cd repo
+    git checkout gh-pages
+    cp -rf "$CWD/test/diffs" "diffs"
+
+    git config user.name "$GIT_USERNAME"
+    git config user.email "$GIT_EMAIL"
+    git config credential.helper "store --file=.git/credentials"
+    echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
+    git add diffs/*
+    git commit -m "Add latest tile diffs from ${TRAVIS_BUILD_NUMBER}"
+    git push origin gh-pages
+
+    cd $CWD
+}
