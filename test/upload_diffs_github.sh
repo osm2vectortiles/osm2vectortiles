a19419fbfe507089e9ba48e23f776d3f752a1c79
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-15 20:40:32 +0200

    Fix typos in upload diffs script

--- test/upload_diffs_github.sh
+++ test/upload_diffs_github.sh
@@ -14,16 +14,18 @@ function commit_latest_diffs() {
 
     cd repo
     git checkout gh-pages
-    cp -rf "$CWD/test/diffs" "diffs"
+    cp -rf "$CWD/diff" "diff"
 
     git config user.name "$GIT_USERNAME"
     git config user.email "$GIT_EMAIL"
     git config credential.helper "store --file=.git/credentials"
     echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-    git add diffs/*
+    git add diff/*
     git commit -m "Add latest tile diffs from ${TRAVIS_BUILD_NUMBER}"
     git push origin gh-pages
 
     cd $CWD
 }
+
+commit_latest_diffs
