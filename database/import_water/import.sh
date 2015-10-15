2eecb98a5924426c5fc0c46354e409849c1c6e54
Manuel Roth <manuelroth@hotmail.ch>
2015-10-15 16:56:31 +0200

    Add download and import script

--- database/import_water/import.sh
+++ database/import_water/import.sh
@@ -8,12 +8,23 @@ readonly OSM_USER=${OSM_USER:-osm}
 readonly OSM_PASSWORD=${OSM_PASSWORD:-osm}
 
 readonly DB_HOST=$DB_PORT_5432_TCP_ADDR
+readonly DATADIR=/usr/src/app/data
+
+function download_shp() {
+	mkdir $DATADIR
+	cd $DATADIR
+	wget http://data.openstreetmapdata.com/water-polygons-split-3857.zip
+	unzip water-polygons-split-3857.zip
+	rm water-polygons-split-3857.zip
+}
 
 function import_shp() {
-	shp2pgsql -g way /data/water-polygons-split-3857/water_polygons.shp | psql -h $DB_HOST -p 5432 -U $OSM_USER -W $OSM_PASSWORD $OSM_DB
+	cd $DATADIR
+	shp2pgsql -g way water-polygons-split-3857/water_polygons.shp | psql -h $DB_HOST -p 5432 -U $OSM_USER -W $OSM_PASSWORD -d $OSM_DB
 }
 
 function main() {
+	download_shp
     import_shp
 }
 
