9f75c42ee338c25b025a37283444ac15eeb6cd16
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-20 15:51:36 +0200

    Modify docker compose config

--- database/jobmaker/create_jobs.py
+++ database/jobmaker/create_jobs.py
@@ -11,7 +11,7 @@ def all_descendant_tiles(x, y, zoom, max_zoom):
 
 def tiles_for_zoom_level(zoom_level):
     # Only switzerland for now
-    tiles = all_descendant_tiles(x=32, y=22, zoom=6, max_zoom=zoom_level)
+    tiles = all_descendant_tiles(x=33, y=22, zoom=6, max_zoom=zoom_level)
 
     for tile in tiles:
         if tile.z == zoom_level:
@@ -19,7 +19,7 @@ def tiles_for_zoom_level(zoom_level):
 
 
 if __name__ == '__main__':
-    for tile in tiles_for_zoom_level(10):
+    for tile in tiles_for_zoom_level(8):
         bbox = mercantile.bounds(tile.x, tile.y, tile.z)
         wgs84 = "{} {} {} {}".format(bbox.west, bbox.south, bbox.east, bbox.north)
         print(wgs84)
