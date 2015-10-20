bfcc04407c07400108aec775e04effcb3359b7e9
Lukas Martinelli <me@lukasmartinelli.ch>
2015-10-20 21:37:10 +0200

    Push jobs to SQS

--- export/jobmaker/create_jobs.py
+++ export/jobmaker/create_jobs.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import json
+
 import mercantile
+import boto.sqs
+from boto.sqs.message import Message
+
+from water import in_water
 
 
 def all_descendant_tiles(x, y, zoom, max_zoom):
@@ -14,12 +23,50 @@ def tiles_for_zoom_level(zoom_level):
     tiles = all_descendant_tiles(x=33, y=22, zoom=6, max_zoom=zoom_level)
 
     for tile in tiles:
-        if tile.z == zoom_level:
+        if tile.z == zoom_level and not in_water(tile.x, tile.y, tile.z):
             yield tile
 
 
+def connect_job_queue():
+    conn = boto.sqs.connect_to_region(
+        region_name=os.getenv('AWS_REGION', 'eu-central-1'),
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY', ''),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY', '')
+    )
+    queue_name = os.getenv('QUEUE_NAME', 'osm2vectortiles_jobs')
+    queue = conn.get_queue(queue_name)
+    return queue
+
+
+def create_job(x, y, min_zoom, max_zoom, bounds):
+    body = {
+        'x': tile.x,
+        'y': tile.y,
+        'min_zoom': tile.z,
+        'max_zoom': 14,
+        'bounds': [
+            [bounds.west, bounds.south],
+            [bounds.east, bounds.north]
+        ]
+    }
+
+    msg = Message()
+    msg.set_body(json.dumps(body))
+    return msg
+
+
 if __name__ == '__main__':
-    for tile in tiles_for_zoom_level(8):
-        bbox = mercantile.bounds(tile.x, tile.y, tile.z)
-        wgs84 = "{} {} {} {}".format(bbox.west, bbox.south, bbox.east, bbox.north)
-        print(wgs84)
+    queue = connect_job_queue()
+    zoom_level = int(os.getenv('TASK_ZOOM_LEVEL', '8'))
+
+    for tile in tiles_for_zoom_level(zoom_level):
+        bounds = mercantile.bounds(tile.x, tile.y, tile.z)
+
+        job = create_job(tile.x, tile.y, min_zoom=0,
+                         max_zoom=tile.z, bounds=bounds)
+        queue.write(job)
+
+        print("{} {} {} {}".format(
+            bounds.west, bounds.south,
+            bounds.east, bounds.north
+        ))
---
+++ export/jobmaker/water.py
@@ -0,0 +1,3 @@
+def in_water(x, y, z):
+    """Detect wether a tile should not be rendered because it is in water"""
+    return False
