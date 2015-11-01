1c21375c8466296e5f2d3b77c9cfcd17d01635f7
Lukas Martinelli <me@lukasmartinelli.ch>
2015-11-01 17:55:05 +0100

    Try to calculate local rank

--- src/prepare-open-streets/functions.sql
+++ src/prepare-open-streets/functions.sql
@@ -80,3 +80,21 @@ BEGIN
     END;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION rank_poi(type VARCHAR) RETURNS INTEGER
+AS $$
+BEGIN
+    RETURN CASE
+        WHEN type IN ('public_building', 'police', 'townhall', 'courthouse') THEN 10
+        WHEN type IN ('stadium') THEN 90
+        WHEN type IN ('hospital') THEN 100
+        WHEN type IN ('zoo') THEN 200
+        WHEN type IN ('university', 'school', 'college', 'kindergarten') THEN 300
+        WHEN type IN ('park') THEN 350
+        WHEN type IN ('supermarket', 'department_store') THEN 400
+        WHEN type IN ('nature_reserve', 'swimming_area') THEN 500
+        WHEN type IN ('attraction') THEN 600
+        ELSE 1000
+    END;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
