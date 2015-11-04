d4900cdcc8723d3e3b0ad479520c1095c782a475
Lukas Martinelli <me@lukasmartinelli.ch>
2015-11-04 21:29:26 +0100

    Better naming for SQL import

--- src/import-sql/functions.sql
+++ src/import-sql/functions.sql
@@ -15,3 +15,28 @@ BEGIN
     END;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION rank_country(population INTEGER) RETURNS INTEGER
+AS $$
+BEGIN
+    RETURN CASE
+        WHEN population >= 250000000 THEN 1
+        WHEN population >= 100000000 THEN 2
+        WHEN population >= 50000000 THEN 3
+        WHEN population >= 25000000 THEN 4
+        WHEN population >= 10000000 THEN 5
+        WHEN population < 10000000 THEN 6
+    END;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION rank_marine(type VARCHAR) RETURNS INTEGER
+AS $$
+BEGIN
+    RETURN CASE
+        WHEN type IN ('ocean') THEN 1
+        WHEN type IN ('sea') THEN 3
+        WHEN type IN ('bay') THEN 5
+    END;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
