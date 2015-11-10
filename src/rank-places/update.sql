7cb0e0c5bde8629deaac607ee08fd0c9188147c6
Lukas Martinelli <me@lukasmartinelli.ch>
2015-11-10 09:39:55 +0100

    Update marine and countries

--- src/rank-places/update.sql
+++ src/rank-places/update.sql
@@ -2,11 +2,32 @@ UPDATE osm_places
 SET scalerank = improved_places.scalerank
 FROM
 (
-    SELECT op.osm_id, nc.scalerank
-    FROM ne_cities AS nc, osm_places AS op
+    SELECT osm.osm_id, ne.scalerank
+    FROM ne_cities AS ne, osm_places AS osm
     WHERE
-        (nc.name = op.name OR nc.name = op.name_en)
-        AND (op.type = 'city' OR op.type = 'town' OR op.type = 'village')
-        AND st_dwithin(nc.geom, op.geometry, 50000)
+        (ne.name LIKE osm.name OR ne.name LIKE osm.name_en)
+        AND (osm.type = 'city' OR osm.type = 'town' OR osm.type = 'village')
+        AND st_dwithin(ne.geom, osm.geometry, 50000)
 ) AS improved_places
-WHERE osm_places.osm_id = improved_places.osm_id
+WHERE osm_places.osm_id = improved_places.osm_id;
+
+UPDATE osm_marine
+SET scalerank = improved_seas.scalerank
+FROM
+(
+    SELECT osm.osm_id, ne.scalerank
+    FROM ne_110m_geography_marine_polys AS ne, osm_marine AS osm
+    WHERE ne.name LIKE osm.name
+) AS improved_seas
+WHERE osm_marine.osm_id = improved_seas.osm_id;
+
+UPDATE osm_countries
+SET scalerank = improved_countries.scalerank
+FROM
+(
+    SELECT osm.osm_id, ne.scalerank
+    FROM ne_110m_admin_0_countries AS ne, osm_countries AS osm
+    WHERE (ne.name LIKE osm.name OR ne.name LIKE osm.name_en)
+) AS improved_countries
+WHERE osm_countries.osm_id = improved_countries.osm_id;
+
