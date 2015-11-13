b0b1b7126974df111b638c5fafcc01e96f128702
Manuel Roth <manuelroth@hotmail.ch>
2015-11-13 16:54:08 +0100

    Remove field scalerank of table water_polygons

--- src/update-scaleranks/update.sql
+++ src/update-scaleranks/update.sql
@@ -15,18 +15,3 @@ FROM
     AND st_dwithin(ne.geom, osm.geometry, 50000)
     ) AS improved_places
 WHERE osm_places.osm_id = improved_places.osm_id;
-
-UPDATE osm_water_polygons
-SET scalerank = improved_lakes.scalerank
-FROM
-(
-    SELECT osm.osm_id, ne.scalerank
-    FROM ne_10m_lakes AS ne, osm_countries AS osm
-    WHERE (
-        ne.name ILIKE osm.name OR
-        ne.name ILIKE osm.name_en OR
-        ne.name_alt ILIKE osm.name OR
-        ne.name_alt ILIKE osm.name_en
-    )
-) AS improved_lakes
-WHERE osm_water_polygons.osm_id = improved_lakes.osm_id;
