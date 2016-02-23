e5e7c73a538f46fb8471417731d1aed8a2f820cf
Manuel Roth <manuelroth@hotmail.ch>
2016-02-23 15:13:32 +0100

    Switch pg node module

--- src/detect-dirty-tiles/updated-geometries.js
+++ src/detect-dirty-tiles/updated-geometries.js
@@ -1,6 +1,22 @@
-var pg = require('pg');
-var conString = "postgres://osm:osm@localhost/osm";
+var pg = require('promise-pg');
+var db = pgp({
+    host: process.env.DB_HOST || 'postgis',
+    port: 5432,
+    database: process.env.DB_NAME || 'osm',
+    user: process.env.DB_USER ||  'osm',
+    password: process.env.DB_PASSWORD ||  'osm'
+});
 
-exports.getUpdatedGeometries = function(tableName, zoomLevel){
+exports.getUpdatedGeometries = function(tableName, zoomLevel, callback){
+	var dbQuery = db.query(
+        "SELECT id, name FROM public.communities_search WHERE name ILIKE '$1^%' OR zip::text = $1",
+        searchQuery
+    );
 
+    dbQuery.then(function(data) {
+    	callback(data);
+    })
+    .catch(function(error) {
+    	console.error(error);
+    })
 }
