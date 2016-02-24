4db5524ea9278c3ecbd738b2bfaab78990bf5ede
Lukas Martinelli <me@lukasmartinelli.ch>
2016-02-24 15:27:21 +0100

    Implement landuse

--- src/detect-dirty-tiles/index.js
+++ src/detect-dirty-tiles/index.js
@@ -1,6 +1,6 @@
 var updatedGeometries = require('./updated-geometries.js');
 
-updatedGeometries.getUpdatedGeometries('osm_poi_points', 14).then(function(geometries) {
+updatedGeometries.getUpdatedGeometries().then(function(geometries) {
     console.log(JSON.stringify(geometries, null, 2));
 }).catch(function(err) {
     console.error(err);
--- src/detect-dirty-tiles/updated-geometries.js
+++ src/detect-dirty-tiles/updated-geometries.js
@@ -1,5 +1,8 @@
+var q = require('q');
+var _ = require('underscore');
 var pgp = require('pg-promise')();
 var tileCover = require('./tile-cover.js');
+
 var db = pgp({
     host: process.env.DB_PORT_5432_TCP_ADDR,
     port: 5432,
@@ -8,31 +11,107 @@ var db = pgp({
     password: 'osm'
 });
 
-function updatedPoiPoints(timestamp) {
-    return db.query('SELECT osm_id, ST_AsGeoJSON(ST_Transform(geometry, 4326)) AS geometry FROM osm_poi_points WHERE timestamp=${timestamp}', { timestamp: timestamp }).then(function(rows) {
+function parseRow(row) {
+    return {
+        osmId: row.osm_id,
+        geometry: JSON.parse(row.geometry)
+    };
+}
+
+function queryChanges(timestamp, tableName, constraints) {
+    return db.query(
+        'SELECT osm_id, ST_AsGeoJSON(ST_Transform(geometry, 4326)) AS geometry ' +
+        'FROM ' + tableName + ' ' +
+        'WHERE timestamp=$1 AND ' + constraints, [timestamp]
+    ).then(function(rows) {
+        return rows.map(parseRow);
+    });
+}
+
+function affectedTiles(timestamp, tableName, zoomLevel, constraints) {
+    return queryChanges(timestamp, tableName, constraints).then(function(rows) {
         return rows.map(function(row) {
             return {
-                osmId: rows.osm_id,
-                geometry: JSON.parse(row.geometry)
+                table: tableName,
+                osmId: row.osmId,
+                affectedTiles: tileCover.coveredTiles(row.geometry, zoomLevel)
             };
         });
     });
 }
 
-exports.getUpdatedGeometries = function(tableName, zoomLevel) {
+function pointOfInterests(timestamp) {
+    return q.all(
+        affectedTiles(timestamp, 'osm_poi_points', 14, "name <> ''"),
+        affectedTiles(timestamp, 'osm_poi_polygons', 14, "name <> ''")
+    ).then(_.flatten);
+}
 
-    if (tableName == 'osm_poi_points' && zoomLevel == 14) {
-        return db
-            .one('SELECT MAX(timestamp) FROM osm_poi_points')
-            .then(function(result) { return updatedPoiPoints(result.max); })
-            .then(function(poiPoints) {
-                return poiPoints.map(function(poiPoint) {
-                    return {
-                        osmId: poiPoint.osm_id,
-                        table: 'osm_poi_points',
-                        affectedTiles: tileCover.coveredTiles(poiPoint.geometry, zoomLevel)
-                    };
-                });
-            });
+function landuse(timestamp) {
+    function hasForest() {
+        return "type IN ('wood', 'nature_reserve', 'national_park', 'forest')";
     }
+
+    function hasNoOverlay() {
+        return "type NOT IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat')";
+    }
+
+    function hasLargerArea(areaThreshold) {
+        return 'st_area(geometry) > ' + areaThreshold;
+    }
+
+    function combine(constraint1, constraint2) {
+        return constraint1 + ' AND ' + constraint2;
+    }
+
+    return q.all([
+        affectedTiles(
+            timestamp, 'osm_landusages_gen0', 5,
+            combine(hasForest(), hasLargerArea(300000000))
+        ),
+        affectedTiles(
+            timestamp, 'osm_landusages_gen0', 6,
+            combine(hasForest(), hasLargerArea(100000000))
+        ),
+        affectedTiles(
+            timestamp, 'osm_landusages_gen0', 7,
+            combine(hasForest(), hasLargerArea(25000000))
+        ),
+        affectedTiles(
+            timestamp, 'osm_landusages_gen0', 8,
+            combine(hasForest(), hasLargerArea(5000000))
+        ),
+        affectedTiles(
+            timestamp, 'osm_landusages_gen0', 9,
+            combine(hasForest(), hasLargerArea(2000000))
+        ),
+        affectedTiles(
+            timestamp, 'osm_landusages_gen0', 10,
+            combine(hasForest(), hasLargerArea(500000))
+        ),
+        affectedTiles(
+            timestamp, 'osm_landusages_gen1', 11,
+            combine(hasForest(), hasLargerArea(100000))
+        ),
+        affectedTiles(
+            timestamp, 'osm_landusages', 12,
+            combine(hasForest(), hasLargerArea(10000))
+        ),
+        affectedTiles(
+            timestamp, 'osm_landusages', 13,
+            hasNoOverlay()
+        )
+    ]).then(_.flatten);
+}
+
+exports.getUpdatedGeometries = function() {
+    return db
+        .one('SELECT MAX(timestamp) FROM osm_poi_points')
+        .then(function(result) { return result.max; })
+        .then(function(timestamp) {
+            return q.all([
+                pointOfInterests(timestamp),
+                landuse(timestamp)
+            ]);
+        }).then(_.flatten);
 };
