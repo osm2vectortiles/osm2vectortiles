eff5d4ac060fd630dcc5c0f11f3e69ebcc93d6d6
Manuel Roth <manuelroth@hotmail.ch>
2016-02-25 17:11:11 +0100

    Add view for layer place_label and alter source query

--- src/detect-dirty-tiles/index.js
+++ src/detect-dirty-tiles/index.js
@@ -28,6 +28,14 @@ function recentDirtyViews() {
         .then(function(result) { return result.max; })
         .then(function(timestamp) {
             return q.all([
+                detectDirtyTiles('place_label_z4toz5', timestamp, 4, 5),
+                detectDirtyTiles('place_label_z6', timestamp, 6, 6),
+                detectDirtyTiles('place_label_z7', timestamp, 7, 7),
+                detectDirtyTiles('place_label_z8', timestamp, 8, 8),
+                detectDirtyTiles('place_label_z9toz10', timestamp, 9, 10),
+                detectDirtyTiles('place_label_z11toz12', timestamp, 11, 12),
+                detectDirtyTiles('place_label_z13', timestamp, 13, 13),
+                detectDirtyTiles('place_label_z14', timestamp, 14, 14),
                 detectDirtyTiles('water_label_z10', timestamp, 10, 10),
                 detectDirtyTiles('water_label_z11', timestamp, 11, 11),
                 detectDirtyTiles('water_label_z12', timestamp, 12, 12),
