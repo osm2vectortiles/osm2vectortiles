744f802036f7607a8052b17e5eb75d87e1bf35a7
Manuel Roth <manuelroth@hotmail.ch>
2016-02-29 10:53:33 +0100

    Add view for layer admin and alter source query

--- src/import-sql/views.sql
+++ src/import-sql/views.sql
@@ -1,3 +1,14 @@
+CREATE OR REPLACE VIEW admin_z2to6 AS
+    SELECT *
+    FROM osm_admin
+    WHERE maritime = 1
+    AND admin_level = 2;
+
+CREATE OR REPLACE VIEW admin_z7to14 AS
+    SELECT *
+    FROM osm_admin
+    WHERE ( admin_level = 2 OR admin_level = 4 );
+
 CREATE OR REPLACE VIEW place_label_z4toz5 AS
     SELECT *
     FROM osm_places
