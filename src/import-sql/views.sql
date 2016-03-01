3c5485028a0b4a4edf7f3debd2cacb688fdd9ac2
Manuel Roth <manuelroth@hotmail.ch>
2016-03-01 13:16:00 +0100

    Make scale_denominator in some queries more specific (BETWEEN instead of >=)

--- src/import-sql/views.sql
+++ src/import-sql/views.sql
@@ -289,19 +289,22 @@ CREATE OR REPLACE VIEW road_label_z8toz10 AS
 CREATE OR REPLACE VIEW road_label_z11 AS
     SELECT *
     FROM osm_roads
-    WHERE type IN ('motorway_link', 'primary', 'primary_link', 'trunk', 'trunk_link', 'secondary', 'secondary_link')
+    WHERE type IN ('motorway', 'motorway_link', 'primary', 'primary_link', 'trunk', 'trunk_link', 'secondary', 'secondary_link')
       AND (name <> '' OR ref <> '');
 
 CREATE OR REPLACE VIEW road_label_z12toz13 AS
     SELECT *
     FROM osm_roads
-    WHERE type IN ('tertiary', 'tertiary_link', 'residential', 'unclassified', 'living_street', 'pedestrian', 'construction', 'rail', 'monorail', 'narrow_gauge', 'subway', 'tram')
+    WHERE type IN ('motorway', 'motorway_link', 'primary', 'primary_link', 'trunk', 'trunk_link', 'secondary', 'secondary_link',
+        'tertiary', 'tertiary_link', 'residential', 'unclassified', 'living_street', 'pedestrian', 'construction', 'rail', 'monorail', 'narrow_gauge', 'subway', 'tram')
       AND name <> '';
 
 CREATE OR REPLACE VIEW road_label_z14 AS
     SELECT *
     FROM osm_roads
-    WHERE type IN ('service', 'track', 'driveway', 'path', 'cycleway', 'ski', 'steps', 'bridleway', 'footway', 'funicular', 'light_rail', 'preserved')
+    WHERE type IN ('motorway', 'motorway_link', 'primary', 'primary_link', 'trunk', 'trunk_link', 'secondary', 'secondary_link',
+        'tertiary', 'tertiary_link', 'residential', 'unclassified', 'living_street', 'pedestrian', 'construction', 'rail', 'monorail',
+        'narrow_gauge', 'subway', 'tram', 'service', 'track', 'driveway', 'path', 'cycleway', 'ski', 'steps', 'bridleway', 'footway', 'funicular', 'light_rail', 'preserved')
       AND name <> '';
 
 CREATE OR REPLACE VIEW waterway_label_z8toz12 AS
