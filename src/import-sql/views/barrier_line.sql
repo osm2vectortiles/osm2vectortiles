6dde27e50d88a4b89ce053c44465e5b6a0be179c
Manuel Roth <manuelroth@hotmail.ch>
2016-03-01 14:58:43 +0100

    Fix aeroway, barrier_line and landuse views to include timestamp attribute

--- src/import-sql/views/barrier_line.sql
+++ src/import-sql/views/barrier_line.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW barrier_line_z14 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_barrier_lines
     UNION ALL
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_barrier_polygons;
--- src/import-sql/views/landuse.sql
+++ src/import-sql/views/landuse.sql
@@ -1,52 +1,52 @@
 CREATE OR REPLACE VIEW landuse_z5 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages_gen0
     WHERE type IN ('wood', 'nature_reserve', 'national_park', 'forest')
       AND st_area(geometry) > 300000000;
 
 CREATE OR REPLACE VIEW landuse_z6 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages_gen0
     WHERE type IN ('wood', 'nature_reserve', 'national_park', 'forest')
       AND st_area(geometry) > 100000000;
 
 CREATE OR REPLACE VIEW landuse_z7 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages_gen0
     WHERE type IN ('wood', 'nature_reserve', 'national_park', 'forest')
       AND st_area(geometry) > 25000000;
 
 CREATE OR REPLACE VIEW landuse_z8 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages_gen0
     WHERE type IN ('wood', 'nature_reserve', 'national_park', 'forest')
       AND st_area(geometry) > 5000000;
 
 CREATE OR REPLACE VIEW landuse_z9 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages_gen0
     WHERE type IN ('wood', 'nature_reserve', 'national_park', 'forest')
       AND st_area(geometry) > 2000000;
 
 CREATE OR REPLACE VIEW landuse_z10 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages_gen0
     WHERE type IN ('wood', 'nature_reserve', 'national_park', 'forest')
       AND st_area(geometry) > 500000;
 
 CREATE OR REPLACE VIEW landuse_z11 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages_gen1
     WHERE type IN ('wood', 'nature_reserve', 'national_park', 'forest')
       AND st_area(geometry) > 100000;
 
 CREATE OR REPLACE VIEW landuse_z12 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages
     WHERE type IN ('wood', 'nature_reserve', 'national_park', 'forest')
       AND st_area(geometry) > 10000;
 
 CREATE OR REPLACE VIEW landuse_z13toz14 AS
-    SELECT osm_id, geometry, type
+    SELECT *
     FROM osm_landusages
     WHERE type NOT IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat');
