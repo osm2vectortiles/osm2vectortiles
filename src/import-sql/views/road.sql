509a7a08f3ed4f1d376b3ad5c32ee4fa5ea41365
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-11 15:41:59 +0100

    Add changed tiles for road

--- src/import-sql/views/road.sql
+++ src/import-sql/views/road.sql
@@ -55,3 +55,47 @@ CREATE OR REPLACE VIEW layer_road AS (
     UNION
     SELECT osm_id, timestamp, geometry FROM road_z14
 );
+
+CREATE OR REPLACE FUNCTION changed_tiles_road(ts timestamp)
+RETURNS TABLE (x INTEGER, y INTEGER, z INTEGER) AS $$
+BEGIN
+	RETURN QUERY (
+		WITH changed_geometries AS (
+		    SELECT osm_id, geometry FROM layer_road
+		    WHERE timestamp = ts
+		), changed_tiles AS (
+		    SELECT DISTINCT c.osm_id, t.tile_x AS x, t.tile_y AS y, t.tile_z AS z
+		    FROM changed_geometries AS c
+		    INNER JOIN LATERAL overlapping_tiles(c.geometry, 14) AS t ON true
+		)
+
+		SELECT c.x, c.y, c.z FROM road_z14 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 14
+		UNION
+
+		SELECT c.x, c.y, c.z FROM road_z13 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 13
+		UNION
+
+		SELECT c.x, c.y, c.z FROM road_z12 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 12
+		UNION
+
+		SELECT c.x, c.y, c.z FROM road_z11 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 11
+		UNION
+
+		SELECT c.x, c.y, c.z FROM road_z8toz10 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z BETWEEN 8 AND 10
+		UNION
+
+		SELECT c.x, c.y, c.z FROM road_z7 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 7
+		UNION
+
+		SELECT c.x, c.y, c.z FROM road_z5toz6 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z BETWEEN 5 AND 6 
+	);
+END;
+$$ LANGUAGE plpgsql;
+
