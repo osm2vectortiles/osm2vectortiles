bc8507491203aa6688a71050f43e1283ccdb868a
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-11 15:01:23 +0100

    Add changed tiles for place_label

--- src/import-sql/functions.sql
+++ src/import-sql/functions.sql
@@ -209,3 +209,49 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE FUNCTION changed_tiles_place_label(ts timestamp)
+RETURNS TABLE (x INTEGER, y INTEGER, z INTEGER) AS $$
+BEGIN
+	RETURN QUERY (
+		WITH changed_geometries AS (
+		    SELECT osm_id, geometry FROM layer_place_label
+		    WHERE timestamp = ts
+		), changed_tiles AS (
+		    SELECT DISTINCT c.osm_id, t.tile_x AS x, t.tile_y AS y, t.tile_z AS z
+		    FROM changed_geometries AS c
+		    INNER JOIN LATERAL overlapping_tiles(c.geometry) AS t ON true
+		)
+
+		SELECT c.x, c.y, c.z FROM place_label_z14 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 14
+		UNION
+
+		SELECT c.x, c.y, c.z FROM place_label_z13 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 13
+		UNION
+
+		SELECT c.x, c.y, c.z FROM place_label_z11toz12 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z BETWEEN 11 AND 12
+		UNION
+
+		SELECT c.x, c.y, c.z FROM place_label_z9toz10 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z BETWEEN 9 AND 10
+		UNION
+
+		SELECT c.x, c.y, c.z FROM place_label_z8 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 8
+		UNION
+
+		SELECT c.x, c.y, c.z FROM place_label_z7 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 7
+		UNION
+
+		SELECT c.x, c.y, c.z FROM place_label_z6 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 6
+		UNION
+
+		SELECT c.x, c.y, c.z FROM place_label_z4toz5 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z BETWEEN 4 AND 5
+	);
+END;
+$$ LANGUAGE plpgsql;
