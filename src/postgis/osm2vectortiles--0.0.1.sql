0adbe5a3ae1d6cab61726cc06fc81360f3533cf7
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-11 20:06:21 +0100

    Switch extension back to PostgreSQL 9.4

--- src/postgis/osm2vectortiles--0.0.1.sql
+++ src/postgis/osm2vectortiles--0.0.1.sql
@@ -2,7 +2,7 @@
 \echo Use "CREATE EXTENSION osm2vectortiles" to load this file. \quit
 
 CREATE TYPE tile AS (x integer, y integer, z integer);
-CREATE OR REPLACE FUNCTION point_to_tiles(lat double precision, lon double precision, zoom_levels integer)
+CREATE OR REPLACE FUNCTION point_to_tiles(lat double precision, lon double precision, max_zoom_level integer)
     RETURNS SETOF tile
 	AS '$libdir/osm2vectortiles', 'point_to_tiles'
     LANGUAGE C IMMUTABLE STRICT;
