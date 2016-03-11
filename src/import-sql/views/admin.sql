eaff6dcc9a564b86fa0cd46ec8248b56280772e5
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-11 15:38:30 +0100

    Move changed tiles functions into respective layer files

--- src/import-sql/views/admin.sql
+++ src/import-sql/views/admin.sql
@@ -14,3 +14,26 @@ CREATE OR REPLACE VIEW layer_admin AS (
     UNION
     SELECT osm_id, timestamp, geometry FROM admin_z7toz14
 );
+
+CREATE OR REPLACE FUNCTION changed_tiles_admin(ts timestamp)
+RETURNS TABLE (x INTEGER, y INTEGER, z INTEGER) AS $$
+BEGIN
+	RETURN QUERY (
+		WITH changed_geometries AS (
+		    SELECT osm_id, geometry FROM layer_admin
+		    WHERE timestamp = ts
+		), changed_tiles AS (
+		    SELECT DISTINCT c.osm_id, t.tile_x AS x, t.tile_y AS y, t.tile_z AS z
+		    FROM changed_geometries AS c
+		    INNER JOIN LATERAL overlapping_tiles(c.geometry) AS t ON true
+		)
+
+		SELECT c.x, c.y, c.z FROM admin_z7toz14 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z BETWEEN 7 AND 14
+		UNION
+
+		SELECT c.x, c.y, c.z FROM admin_z2toz6 AS l
+		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z BETWEEN 2 AND 6
+	);
+END;
+$$ LANGUAGE plpgsql;
