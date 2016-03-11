dff64d77fbc99cab8c6e25411995ff283cda6878
Manuel Roth <manuelroth@hotmail.ch>
2016-03-11 11:09:18 +0100

    Correct inconsistent view names

--- src/import-sql/views/admin.sql
+++ src/import-sql/views/admin.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE VIEW admin_z2to6 AS
+CREATE OR REPLACE VIEW admin_z2toz6 AS
     SELECT *
     FROM osm_admin
     WHERE maritime = 1
       AND admin_level = 2;
 
-CREATE OR REPLACE VIEW admin_z7to14 AS
+CREATE OR REPLACE VIEW admin_z7toz14 AS
     SELECT *
     FROM osm_admin
     WHERE ( admin_level = 2 OR admin_level = 4 );
