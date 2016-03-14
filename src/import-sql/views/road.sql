d08ec998d4b764b2f6dfc0944f5d4e1d03405a7a
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-14 13:51:39 +0100

    Unify roads into osm_road_unique view

--- src/import-sql/views/road.sql
+++ src/import-sql/views/road.sql
@@ -1,26 +1,44 @@
+CREATE OR REPLACE VIEW osm_unique_road AS (
+    SELECT rp.osm_id, rp.geometry, rp.type, rp.service,
+           rp.oneway, rp.is_bridge, rp.is_tunnel, rp.timestamp,
+           rp.ref, rp.name, rp.name_en, rp.name_es,
+           rp.name_fr, rp.name_de, rp.name_ru, rp.name_zh,
+           rp.layer, rp.z_order
+    FROM osm_road_polygon AS rp
+    UNION ALL
+    SELECT rl.osm_id, rl.geometry, rl.type, rl.service,
+           rl.oneway, rl.is_bridge, rl.is_tunnel, rl.timestamp,
+           rl.ref, rl.name, rl.name_en, rl.name_es,
+           rl.name_fr, rl.name_de, rl.name_ru, rl.name_zh,
+           rl.layer, rp.z_order
+    FROM osm_road_linestring AS rl
+    LEFT JOIN osm_road_polygon AS rp ON rp.osm_id = rl.osm_id
+    WHERE rp.osm_id IS NULL
+);
+
 CREATE OR REPLACE VIEW road_z5toz6 AS
     SELECT *
-    FROM osm_roads
+    FROM osm_unique_road
     WHERE type IN ('motorway', 'trunk');
 
 CREATE OR REPLACE VIEW road_z7 AS
     SELECT *
-    FROM osm_roads
+    FROM osm_unique_road
     WHERE type IN ('motorway', 'trunk', 'primary', 'motorway_link', 'primary_link', 'trunk_link');
 
 CREATE OR REPLACE VIEW road_z8toz10 AS
     SELECT *
-    FROM osm_roads
+    FROM osm_unique_road
     WHERE type IN ('motorway', 'trunk', 'primary', 'motorway_link', 'primary_link', 'trunk_link', 'secondary', 'secondary_link');
 
 CREATE OR REPLACE VIEW road_z11 AS
     SELECT *
-    FROM osm_roads
+    FROM osm_unique_road
     WHERE type IN ('motorway', 'trunk', 'primary', 'motorway_link', 'primary_link', 'trunk_link', 'secondary', 'secondary_link', 'tertiary', 'teriary_link');
 
 CREATE OR REPLACE VIEW road_z12 AS
     SELECT *
-    FROM osm_roads
+    FROM osm_unique_road
     WHERE classify_road(type) IN ('major_rail', 'street')
       AND type IN ('motorway', 'trunk', 'primary', 'motorway_link', 'primary_link', 'trunk_link', 'secondary', 'secondary_link', 'tertiary', 'teriary_link')
       AND NOT is_bridge
@@ -28,7 +46,7 @@ CREATE OR REPLACE VIEW road_z12 AS
 
 CREATE OR REPLACE VIEW road_z13 AS
     SELECT *
-    FROM osm_roads
+    FROM osm_unique_road
     WHERE classify_road(type) IN ('minor_rail', 'street_limited', 'service')
       AND type IN ('motorway', 'trunk', 'primary', 'motorway_link', 'primary_link', 'trunk_link', 'secondary', 'secondary_link', 'tertiary', 'teriary_link')
       AND NOT is_bridge
@@ -36,7 +54,7 @@ CREATE OR REPLACE VIEW road_z13 AS
 
 CREATE OR REPLACE VIEW road_z14 AS
     SELECT *
-    FROM osm_roads
+    FROM osm_unique_road
     WHERE NOT is_bridge
       AND NOT is_tunnel;
 
