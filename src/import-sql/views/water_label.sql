4366885440c42b41ce7b822415f1f731be6bb0ae
Manuel Roth <manuelroth@hotmail.ch>
2016-03-14 09:31:38 +0100

    Water_label: Move conversion of polygon to point to view

--- src/import-sql/views/water_label.sql
+++ src/import-sql/views/water_label.sql
@@ -1,25 +1,25 @@
 CREATE OR REPLACE VIEW water_label_z10 AS
-    SELECT *
+    SELECT osm_id, topoint(geometry), area, name, name_en, name_es, name_fr, name_de, name_ru, name_zh, timestamp
     FROM osm_water_polygons
     WHERE area >= 100000000;
 
 CREATE OR REPLACE VIEW water_label_z11 AS
-    SELECT *
+    SELECT osm_id, topoint(geometry), area, name, name_en, name_es, name_fr, name_de, name_ru, name_zh, timestamp
     FROM osm_water_polygons
     WHERE area >= 40000000;
 
 CREATE OR REPLACE VIEW water_label_z12 AS
-    SELECT *
+    SELECT osm_id, topoint(geometry), area, name, name_en, name_es, name_fr, name_de, name_ru, name_zh, timestamp
     FROM osm_water_polygons
     WHERE area >= 20000000;
 
 CREATE OR REPLACE VIEW water_label_z13 AS
-    SELECT *
+    SELECT osm_id, topoint(geometry), area, name, name_en, name_es, name_fr, name_de, name_ru, name_zh, timestamp
     FROM osm_water_polygons
     WHERE area >= 10000000;
 
 CREATE OR REPLACE VIEW water_label_z14 AS
-    SELECT *
+    SELECT osm_id, topoint(geometry), area, name, name_en, name_es, name_fr, name_de, name_ru, name_zh, timestamp
     FROM osm_water_polygons;
 
 CREATE OR REPLACE VIEW layer_water_label AS (
