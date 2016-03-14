1dde294ce1193659fef0ef7b9b1897440a7d4ac5
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-14 09:26:36 +0100

    Refactor changed_tiles_landuse_overlay

--- src/import-sql/views/landuse_overlay.sql
+++ src/import-sql/views/landuse_overlay.sql
@@ -1,36 +1,39 @@
+CREATE OR REPLACE FUNCTION is_landuse_overlay(type TEXT)
+RETURNS BOOLEAN AS $$
+BEGIN
+	RETURN type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat');
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE VIEW landuse_overlay_z7 AS
     SELECT *
     FROM osm_landusages_gen0
-    WHERE type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat')
-      AND st_area(geometry) > 20000000;
+    WHERE is_landuse_overlay(type) AND st_area(geometry) > 20000000;
 
 CREATE OR REPLACE VIEW landuse_overlay_z8 AS
     SELECT *
     FROM osm_landusages_gen0
-    WHERE type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat')
-      AND st_area(geometry) > 6000000;
+    WHERE is_landuse_overlay(type) AND st_area(geometry) > 6000000;
 
 CREATE OR REPLACE VIEW landuse_overlay_z9 AS
     SELECT *
     FROM osm_landusages_gen0
-    WHERE type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat')
-      AND st_area(geometry) > 2000000;
+    WHERE is_landuse_overlay(type) AND st_area(geometry) > 2000000;
 
 CREATE OR REPLACE VIEW landuse_overlay_z10 AS
     SELECT *
     FROM osm_landusages_gen0
-    WHERE type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat')
-      AND st_area(geometry) > 500000;
+    WHERE is_landuse_overlay(type) AND st_area(geometry) > 500000;
 
 CREATE OR REPLACE VIEW landuse_overlay_z11toz12 AS
     SELECT *
     FROM osm_landusages_gen1
-    WHERE type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat');
+    WHERE is_landuse_overlay(type);
 
 CREATE OR REPLACE VIEW landuse_overlay_z13toz14 AS
     SELECT *
     FROM osm_landusages
-    WHERE type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat');
+    WHERE is_landuse_overlay(type);
 
 CREATE OR REPLACE VIEW layer_landuse_overlay AS (
     SELECT osm_id, timestamp, geometry FROM landuse_overlay_z7
