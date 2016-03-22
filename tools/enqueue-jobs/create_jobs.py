bd9652cf1db78260f6a3189989a8f46bd726bb47
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-22 16:53:58 +0100

    Extend create_jobs.py for list batch jobs

--- tools/enqueue-jobs/create_jobs.py
+++ tools/enqueue-jobs/create_jobs.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+"""Generate jobs for rendering tiles in pyramid and list format in JSON format
+
+Usage:
+  generate_jobs.py pyramid <x> <y> <z> --max-zoom=<max-zoom>
+  generate_jobs.py list <list_file> --batch-size=<batch_size>
+  generate_jobs.py (-h | --help)
+  generate_jobs.py --version
+
+Options:
+  -h --help                    Show this screen.
+  --version                    Show version.
+  --max-zoom=<max-zoom>        Max zoom level for pyramid jobs.
+  --batch-size=<batch_size>    Amount of tiles for list jobs.
+"""
 
 import os
 import json
@@ -6,6 +20,7 @@ import json
 import mercantile
 import boto.sqs
 from boto.sqs.message import Message
+from docopt import docopt
 
 
 def all_descendant_tiles(x, y, zoom, max_zoom):
@@ -15,16 +30,6 @@ def all_descendant_tiles(x, y, zoom, max_zoom):
             yield child_tile
             yield from all_descendant_tiles(child_tile.x, child_tile.y,
                                             child_tile.z, max_zoom)
-
-
-def tiles_for_switzerland(zoom_level):
-    tiles = all_descendant_tiles(x=33, y=22, zoom=6, max_zoom=zoom_level)
-    return (t for t in tiles if t.z == zoom_level)
-
-
-def tiles_for_small_europe(zoom_level):
-    tiles = all_descendant_tiles(x=8, y=5, zoom=4, max_zoom=zoom_level)
-    return (t for t in tiles if t.z == zoom_level)
 
 
 def tiles_for_zoom_level(zoom_level):
@@ -45,10 +50,18 @@ def connect_job_queue():
     return queue
 
 
-def create_job(x, y, min_zoom, max_zoom, bounds):
-    body = {
+def create_list_batch_job(tile_list):
+    return {
+        'type': 'list',
+        'tiles':  tile_list
+    }
+
+
+def create_pyramid_job(x, y, min_zoom, max_zoom, bounds):
+    return {
         'x': tile.x,
         'y': tile.y,
+        'type': 'pyramid',
         'min_zoom': tile.z,
         'max_zoom': 14,
         'bounds': {
@@ -59,23 +72,50 @@ def create_job(x, y, min_zoom, max_zoom, bounds):
         }
     }
 
-    msg = Message()
-    msg.set_body(json.dumps(body))
-    return msg
 
+def split_tiles_into_batch_jobs(tiles):
+    tiles_batch = []
+
+    for line in file_handle:
+        tiles_batch.append(line)
+        if len(job_list) > batch_size:
+            yield create_list_batch_job(tiles_batch)
+            tiles_batch = []
+
+    yield create_list_batch_job(tiles_batch)
+
+
+def pyramid_jobs(x, y, z, max_zoom)
+        tiles = all_descendant_tiles(x=x, y=y, zoom=0, max_zoom=zoom_level)
+        pyramid_zoom_level_tiles = (t for t in tiles if t.z == zoom_level)
+
+        for tile in pyramid_zoom_level_tiles:
+            bounds = mercantile.bounds(tile.x, tile.y, tile.z)
+            yield create_pyramid_job(tile.x, tile.y, min_zoom=0,
+                                     max_zoom=tile.z, bounds=bounds)
 
 if __name__ == '__main__':
-    queue = connect_job_queue()
-    zoom_level = int(os.getenv('TASK_ZOOM_LEVEL', '8'))
+    args = docopt(__doc__, version='0.1')
 
-    for tile in tiles_for_switzerland(zoom_level):
-        bounds = mercantile.bounds(tile.x, tile.y, tile.z)
+    if  args['pyramid']:
+        zoom_level = int(args['<max-zoom>'])
 
-        job = create_job(tile.x, tile.y, min_zoom=0,
-                         max_zoom=tile.z, bounds=bounds)
-        queue.write(job)
+        x = int(args['<x>'])
+        y = int(args['<y>'])
+        z = int(args['<z>'])
 
-        print("{} {} {} {}".format(
-            bounds.west, bounds.south,
-            bounds.east, bounds.north
-        ))
+        for job in pyramid_jobs(x, y, z, zoom_level):
+            print(job)
+
+    if  args['list']:
+        batch_size = int(args['<batch_size>'])
+
+        tiles = []
+        with open(args['list_file'],"r") as file_handle:
+            for line in file_handle:
+                tiles.append(line)
+
+        for job in split_tiles_into_batch_jobs(tiles):
+            print(job)
+
+
