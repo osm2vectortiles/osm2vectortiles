10a934e7d761ab7855bec8a78730a4e71d3276e4
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-29 14:35:31 +0200

    Switch from tl copy to tilelive-copy

--- src/export/export-local.sh
+++ src/export/export-local.sh
@@ -8,16 +8,15 @@ source utils.sh
 readonly RENDER_SCHEME=${RENDER_SCHEME:-pyramid}
 readonly MIN_ZOOM=${MIN_ZOOM:-0}
 readonly MAX_ZOOM=${MAX_ZOOM:-14}
-readonly BBOX=${BBOX:-"-180, -85.0511, 180, 85.0511"}
+readonly BBOX=${BBOX:-"-180,-85.0511,180,85.0511"}
 
 function export_local_mbtiles() {
     local mbtiles_name="tiles.mbtiles"
-
-    exec tl copy \
-        -s pyramid \
-        -b "$BBOX" \
-        --min-zoom="$MIN_ZOOM" \
-        --max-zoom="$MAX_ZOOM" \
+    exec tilelive-copy \
+        --scheme pyramid \
+        --bounds "$BBOX" \
+        --minzoom "$MIN_ZOOM" \
+        --maxzoom "$MAX_ZOOM" \
         "tmsource://$DEST_PROJECT_DIR" "mbtiles://$EXPORT_DIR/$mbtiles_name"
 }
 
