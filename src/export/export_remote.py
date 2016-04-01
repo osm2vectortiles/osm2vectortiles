fbe1884d18649afaf3f0a5f7d74760f94d81cb9e
lukasmartinelli <me@lukasmartinelli.ch>
2016-04-01 10:30:31 +0200

    Remove pgbouncer as benefit is minimal and troubles often big

--- src/export/export_remote.py
+++ src/export/export_remote.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Wrapper around tilelive for exporting vector tiles from tm2source.
+"""Wrapper around tilelive-copy for exporting vector tiles from tm2source.
 
 Usage:
   export_remote.py <rabbitmq_url> --tm2source=<tm2source> [--job-queue=<job-queue>] [--bucket=<bucket>] [--render_scheme=<scheme>]
@@ -15,10 +15,7 @@ Options:
   --bucket=<bucket>         S3 Bucket name for storing results [default: osm2vectortiles-jobs]
 
 """
-import watchtower
-import socket
 import time
-import logging
 import subprocess
 import re
 import sys
@@ -27,50 +24,13 @@ import os.path
 import json
 
 from boto.s3.connection import S3Connection, OrdinaryCallingFormat
-import boto.sqs
 import pika
 from docopt import docopt
 
 
-logging.basicConfig(level=logging.INFO)
-mapnik_logger = logging.getLogger("mapnik")
-export_logger = logging.getLogger("export")
-
-
-def local_ip():
-    return socket.gethostbyname(socket.gethostname())
-
-
-def create_tilelive_command(tm2source, mbtiles_file, bbox,
-                            min_zoom=8, max_zoom=12, scheme='pyramid'):
-    tilelive_binary = os.getenv('TILELIVE_BIN', 'tilelive-copy')
-    source = 'tmsource://' + os.path.abspath(tm2source)
-    sink = 'mbtiles://' + os.path.abspath(mbtiles_file)
-
-    cmd = [
-        tilelive_binary,
-        '--scheme', 'pyramid',
-        '--bounds', bbox,
-        '--minzoom', str(min_zoom),
-        '--maxzoom', str(max_zoom),
-        source, sink
-    ]
-
-    return cmd
-
-
-def connect_job_queue(queue_name):
-    conn = boto.sqs.connect_to_region(os.getenv('AWS_REGION', 'eu-central-1'))
-    queue = conn.get_queue(queue_name)
-    if queue is None:
-        raise ValueError('Could not connect to queue {}'.format(queue_name))
-    return queue
-
-
 def connect_s3(bucket_name):
-    #boto.set_stream_logger('paws')
+    # boto.set_stream_logger('paws')
     port = int(os.getenv('AWS_S3_PORT', 8080))
-    print(port)
     is_secure = port == 443
     conn = S3Connection(
         os.getenv('AWS_ACCESS_KEY_ID', 'dummy'),
@@ -86,26 +46,29 @@ def connect_s3(bucket_name):
 
 
 def upload_mbtiles(bucket, mbtiles_file):
+    """Upload mbtiles file to a bucket with the filename as S3 key"""
     keyname = os.path.basename(mbtiles_file)
     obj = bucket.new_key(keyname)
     obj.set_contents_from_filename(mbtiles_file)
 
 
 def create_tilelive_bbox(bounds):
-    return = '{} {} {} {}'.format(
+    return '{},{},{},{}'.format(
         bounds['west'], bounds['south'],
         bounds['east'], bounds['north']
     )
-    
-def render_tile_list_command(source, sink, link_file)
-    return = [
+
+
+def render_tile_list_command(source, sink, list_file):
+    return [
         'tilelive-copy',
         '--scheme', 'list',
         '--list', list_file,
         source, sink
     ]
 
-def render_pyramid_command(source, sink, bounds, min_zoom, max_zoom)
+
+def render_pyramid_command(source, sink, bounds, min_zoom, max_zoom):
     return [
         'tilelive-copy',
         '--scheme', 'pyramid',
@@ -114,6 +77,7 @@ def render_pyramid_command(source, sink, bounds, min_zoom, max_zoom)
         '--maxzoom', str(max_zoom),
         source, sink
     ]
+
 
 def export_remote(tm2source, rabbitmq_url, queue_name, render_scheme, bucket_name):
     bucket = connect_s3(bucket_name)
@@ -124,28 +88,31 @@ def export_remote(tm2source, rabbitmq_url, queue_name, render_scheme, bucket_nam
 
     def callback(ch, method, properties, body):
         msg = json.loads(body.decode('utf-8'))
+        print(msg)
         task_id = msg['id']
         mbtiles_file = task_id + '.mbtiles'
 
         source = 'tmsource://' + os.path.abspath(tm2source)
         sink = 'mbtiles://' + os.path.abspath(mbtiles_file)
-        tileinfo = msg['tile']
         tilelive_cmd = []
 
-        if body['type'] == 'pyramid':
+        if msg['type'] == 'pyramid':
+            pyramid = msg['pyramid']
+            tileinfo = pyramid['tile']
+
             tilelive_cmd = render_pyramid_command(
-                source, sink,          
-                bounds=create_tilelive_bbox(tile['bounds']),
+                source, sink,
+                bounds=create_tilelive_bbox(pyramid['bounds']),
                 min_zoom=tileinfo['min_zoom'],
                 max_zoom=tileinfo['max_zoom']
             )
-        elif body['type'] == 'list':
+        elif msg['type'] == 'list':
             list_file='/tmp/tiles.txt'
             with open(list_file, 'w') as fh:
                 write_list_file(fh)
 
             tilelive_cmd = render_tile_list_command(
-                source, sink,          
+                source, sink,
                 list_file=list_file,
             )
         else:
@@ -158,15 +125,12 @@ def export_remote(tm2source, rabbitmq_url, queue_name, render_scheme, bucket_nam
         )
         end = time.time()
 
-        export_logger.info('Elapsed time: {}'.format(int(end - start)),
-                           extra=logging_info)
+        print('Elapsed time: {}'.format(int(end - start)))
+
         upload_mbtiles(bucket, mbtiles_file)
         os.remove(mbtiles_file)
-        export_logger.info('Upload mbtiles {}'.format(mbtiles_file),
-                           extra=logging_info)
 
-
-
+        print('Upload mbtiles {}'.format(mbtiles_file))
 
 
     channel.basic_consume(callback, queue=queue_name)
@@ -190,6 +154,10 @@ def configure_rabbitmq(channel):
 
 
 def durable_publish(channel, queue, body):
+    """
+    Publish a message body to a queue in a channel and ensure it stays
+    durable on RabbitMQ server restart
+    """
     properties = pika.BasicProperties(delivery_mode=2)
     channel.basic_publish(exchange='', routing_key=queue,
                           body=body, properties=properties)
@@ -205,13 +173,6 @@ def write_list_file(fh, tiles):
 
 
 def main(args):
-    formatter = logging.Formatter('%(ip)s %(task_id)s %(message)s')
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(formatter)
-
-    mapnik_logger.addHandler(handler)
-    export_logger.addHandler(handler)
-
     export_remote(
         args['--tm2source'],
         args['<rabbitmq_url>'],
