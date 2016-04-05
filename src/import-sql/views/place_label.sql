a47b8dee6d7bbc967db5e1c28b9c2feb213403df
lukasmartinelli <me@lukasmartinelli.ch>
2016-04-05 13:22:00 +0200

    Check if name is null for place labels

--- src/import-sql/views/place_label.sql
+++ src/import-sql/views/place_label.sql
@@ -1,51 +1,51 @@
 CREATE OR REPLACE VIEW place_label_z4toz5 AS
     SELECT *
     FROM osm_place_point
-    WHERE name <> ''
+    WHERE name IS NOT NULL
       AND scalerank IS NOT NULL
       AND scalerank < 3;
 
 CREATE OR REPLACE VIEW place_label_z6 AS
     SELECT *
     FROM osm_place_point
-    WHERE name <> ''
+    WHERE name IS NOT NULL
       AND scalerank IS NOT NULL
       AND scalerank < 8;
 
 CREATE OR REPLACE VIEW place_label_z7 AS
     SELECT *
     FROM osm_place_point
-    WHERE name <> ''
+    WHERE name IS NOT NULL
       AND scalerank IS NOT NULL;
 
 CREATE OR REPLACE VIEW place_label_z8 AS
     SELECT *
     FROM osm_place_point
-    WHERE name <> ''
+    WHERE name IS NOT NULL
       AND type IN ('city', 'town');
 
 CREATE OR REPLACE VIEW place_label_z9toz10 AS
     SELECT *
     FROM osm_place_point
-    WHERE name <> ''
+    WHERE name IS NOT NULL
       AND type IN ('city', 'town', 'district');
 
 CREATE OR REPLACE VIEW place_label_z11toz12 AS
     SELECT *
     FROM osm_place_point
-    WHERE name <> ''
+    WHERE name IS NOT NULL
       AND type IN ('city', 'town', 'district', 'village');
 
 CREATE OR REPLACE VIEW place_label_z13 AS
     SELECT *
     FROM osm_place_point
-    WHERE name <> ''
+    WHERE name IS NOT NULL
       AND type IN ('city', 'town', 'district', 'village', 'hamlet', 'suburb','neighbourhood');
 
 CREATE OR REPLACE VIEW place_label_z14 AS
     SELECT *
     FROM osm_place_point
-    WHERE name <> '';
+    WHERE name IS NOT NULL;
 
 CREATE OR REPLACE VIEW layer_place_label AS (
     SELECT osm_id, timestamp, geometry FROM place_label_z4toz5
