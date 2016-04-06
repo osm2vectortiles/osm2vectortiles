a06efbee8bd8942482fa0a58953423892be9faf5
Manuel Roth <manuelroth@hotmail.ch>
2016-04-06 17:23:17 +0200

    Move national_parks and similar from layer landuse to layer landuse_overlay

--- src/import-sql/views/landuse_overlay.sql
+++ src/import-sql/views/landuse_overlay.sql
@@ -1,9 +1,19 @@
 CREATE OR REPLACE FUNCTION is_landuse_overlay(type TEXT)
 RETURNS BOOLEAN AS $$
 BEGIN
-	RETURN type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat');
+	RETURN type IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat', 'national_park', 'nature_reserve', 'protected_area');
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE VIEW landuse_overlay_z5 AS
+    SELECT *
+    FROM osm_landuse_polygon_gen0
+    WHERE is_landuse_overlay(type) AND st_area(geometry) > 300000000;
+
+CREATE OR REPLACE VIEW landuse_overlay_z6 AS
+    SELECT *
+    FROM osm_landuse_polygon_gen0
+    WHERE is_landuse_overlay(type) AND st_area(geometry) > 100000000;
 
 CREATE OR REPLACE VIEW landuse_overlay_z7 AS
     SELECT *
@@ -36,6 +46,10 @@ CREATE OR REPLACE VIEW landuse_overlay_z13toz14 AS
     WHERE is_landuse_overlay(type);
 
 CREATE OR REPLACE VIEW layer_landuse_overlay AS (
+    SELECT osm_id, timestamp, geometry FROM landuse_overlay_z5
+    UNION
+    SELECT osm_id, timestamp, geometry FROM landuse_overlay_z6
+    UNION
     SELECT osm_id, timestamp, geometry FROM landuse_overlay_z7
     UNION
     SELECT osm_id, timestamp, geometry FROM landuse_overlay_z8
@@ -81,6 +95,14 @@ BEGIN
 
 		SELECT c.x, c.y, c.z FROM landuse_overlay_z7 AS l
 		INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 7
+        UNION
+
+        SELECT c.x, c.y, c.z FROM landuse_overlay_z7 AS l
+        INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 6
+        UNION
+
+        SELECT c.x, c.y, c.z FROM landuse_overlay_z7 AS l
+        INNER JOIN changed_tiles AS c ON c.osm_id = l.osm_id AND c.z = 5
 	);
 END;
 $$ LANGUAGE plpgsql;
