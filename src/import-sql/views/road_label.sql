516f9ea0b53cfcf2d0ac469fb288119e00324cf6
lukasmartinelli <me@lukasmartinelli.ch>
2016-04-08 16:31:08 +0200

    Use single table for road geometries #219

--- src/import-sql/views/road_label.sql
+++ src/import-sql/views/road_label.sql
@@ -1,25 +1,25 @@
 CREATE OR REPLACE VIEW road_label_z8toz10 AS
     SELECT *
-    FROM osm_unique_road
+    FROM osm_road_geometry
     WHERE type IN ('motorway')
       AND ref <> '';
 
 CREATE OR REPLACE VIEW road_label_z11 AS
     SELECT *
-    FROM osm_unique_road
+    FROM osm_road_geometry
     WHERE type IN ('motorway', 'motorway_link', 'primary', 'primary_link', 'trunk', 'trunk_link', 'secondary', 'secondary_link')
       AND (name <> '' OR ref <> '');
 
 CREATE OR REPLACE VIEW road_label_z12toz13 AS
     SELECT *
-    FROM osm_unique_road
+    FROM osm_road_geometry
     WHERE type IN ('motorway', 'motorway_link', 'primary', 'primary_link', 'trunk', 'trunk_link', 'secondary', 'secondary_link',
         'tertiary', 'tertiary_link', 'residential', 'unclassified', 'living_street', 'pedestrian', 'construction', 'rail', 'monorail', 'narrow_gauge', 'subway', 'tram')
       AND name <> '';
 
 CREATE OR REPLACE VIEW road_label_z14 AS
     SELECT *
-    FROM osm_unique_road
+    FROM osm_road_geometry
     WHERE type IN ('motorway', 'motorway_link', 'primary', 'primary_link', 'trunk', 'trunk_link', 'secondary', 'secondary_link',
         'tertiary', 'tertiary_link', 'residential', 'unclassified', 'living_street', 'pedestrian', 'construction', 'rail', 'monorail',
         'narrow_gauge', 'subway', 'tram', 'service', 'track', 'driveway', 'path', 'cycleway', 'ski', 'steps', 'bridleway', 'footway', 'funicular', 'light_rail', 'preserved')
