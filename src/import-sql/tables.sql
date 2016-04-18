2c27d356b08f17ae03a1b51fe0a4b17f0fc89f73
lukasmartinelli <me@lukasmartinelli.ch>
2016-04-18 22:23:40 +0200

    All information for trigger mgmt and chagned tiles can be described as data

---
+++ src/import-sql/tables.sql
@@ -0,0 +1,78 @@
+tables:
+  admin_linestring:
+    buffer: 4
+    min_zoom: 2
+    max_zoom: 14
+  aero_linestring:
+    buffer: 4
+    min_zoom: 9
+    max_zoom: 14
+  aero_polygon:
+    buffer: 4
+    min_zoom: 9
+    max_zoom: 14
+  airport_point:
+    buffer: 64
+    min_zoom: 9
+    max_zoom: 14
+  airport_polygon:
+    buffer: 64
+    min_zoom: 9
+    max_zoom: 14
+  barrier_linestring:
+    buffer: 4
+    min_zoom: 14
+    max_zoom: 14
+  barrier_polygon:
+    buffer: 4
+    min_zoom: 14
+    max_zoom: 14
+  building_polygon:
+    buffer: 2
+    min_zoom: 13
+    max_zoom: 14
+  housenumber_point:
+    buffer: 64
+    min_zoom: 14
+    max_zoom: 14
+    buffer: 64
+  housenumber_polygon:
+    buffer: 64
+    min_zoom: 14
+    max_zoom: 14
+  landuse_polygon:
+    buffer: 4
+    min_zoom: 5
+    max_zoom: 14
+  place_geometry:
+    buffer: 128
+    min_zoom: 3
+    max_zoom: 14
+  road_geometry:
+    buffer: 8
+    min_zoom: 5
+    max_zoom: 14
+  rail_station_point:
+    buffer: 64
+    min_zoom: 5
+    max_zoom: 14
+  water_polygon:
+    buffer: 64
+    min_zoom: 5
+    max_zoom: 14
+  water_linestring:
+    buffer: 4
+    min_zoom: 8
+    max_zoom: 14
+  mountain_peak_point:
+    buffer: 64
+    min_zoom: 12
+    max_zoom: 14
+  poi_polygon:
+    buffer: 128
+    min_zoom: 14
+    max_zoom: 14
+  poi_point:
+    buffer: 128
+    min_zoom: 14
+    max_zoom: 14
