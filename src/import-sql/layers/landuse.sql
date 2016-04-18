4444490ccc7c4ca3c6197bbec5fd94d3961f99ac
Manuel Roth <manuelroth@hotmail.ch>
2016-04-18 13:23:28 +0200

    Use classify_landuse instead of type

--- src/import-sql/layers/landuse.sql
+++ src/import-sql/layers/landuse.sql
@@ -1,49 +1,49 @@
 CREATE OR REPLACE VIEW landuse_z5 AS
     SELECT *
     FROM osm_landuse_polygon_gen0
-    WHERE type IN ('wood', 'forest')
+    WHERE classify_landuse(type) IN ('wood')
       AND st_area(geometry) > 300000000;
 
 CREATE OR REPLACE VIEW landuse_z6 AS
     SELECT *
     FROM osm_landuse_polygon_gen0
-    WHERE type IN ('wood', 'forest')
+    WHERE classify_landuse(type) IN ('wood')
       AND st_area(geometry) > 100000000;
 
 CREATE OR REPLACE VIEW landuse_z7 AS
     SELECT *
     FROM osm_landuse_polygon_gen0
-    WHERE type IN ('wood', 'forest')
+    WHERE classify_landuse(type) IN ('wood')
       AND st_area(geometry) > 25000000;
 
 CREATE OR REPLACE VIEW landuse_z8 AS
     SELECT *
     FROM osm_landuse_polygon_gen0
-    WHERE type IN ('wood', 'forest')
+    WHERE classify_landuse(type) IN ('wood')
       AND st_area(geometry) > 5000000;
 
 CREATE OR REPLACE VIEW landuse_z9 AS
     SELECT *
     FROM osm_landuse_polygon_gen0
-    WHERE type IN ('wood', 'forest')
+    WHERE classify_landuse(type) IN ('wood')
       AND st_area(geometry) > 2000000;
 
 CREATE OR REPLACE VIEW landuse_z10 AS
     SELECT *
     FROM osm_landuse_polygon_gen0
-    WHERE type IN ('wood', 'forest')
+    WHERE classify_landuse(type) IN ('wood')
       AND st_area(geometry) > 500000;
 
 CREATE OR REPLACE VIEW landuse_z11 AS
     SELECT *
     FROM osm_landuse_polygon_gen1
-    WHERE type IN ('wood', 'forest')
+    WHERE classify_landuse(type) IN ('wood')
       AND st_area(geometry) > 100000;
 
 CREATE OR REPLACE VIEW landuse_z12 AS
     SELECT *
     FROM osm_landuse_polygon
-    WHERE type IN ('wood', 'forest')
+    WHERE classify_landuse(type) IN ('wood')
       AND st_area(geometry) > 10000;
 
 CREATE OR REPLACE VIEW landuse_z13toz14 AS
