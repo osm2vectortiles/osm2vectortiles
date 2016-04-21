9cc5c2a4a9b52d08f628ab0ec3e82e6740fdb36a
Lukas Martinelli <me@lukasmartinelli.ch>
2016-04-21 10:47:10 +0200

    Show drains and ditches in layer waterway

--- src/import-sql/layers/waterway.sql
+++ src/import-sql/layers/waterway.sql
@@ -3,10 +3,15 @@ CREATE OR REPLACE VIEW waterway_z8toz12 AS
     FROM osm_water_linestring
     WHERE type IN ('river', 'canal');
 
-CREATE OR REPLACE VIEW waterway_z13toz14 AS
+CREATE OR REPLACE VIEW waterway_z13 AS
     SELECT *
     FROM osm_water_linestring
     WHERE type IN ('river', 'canal', 'stream', 'stream_intermittent');
+
+CREATE OR REPLACE VIEW waterway_z14 AS
+    SELECT *
+    FROM osm_water_linestring
+    WHERE type IN ('river', 'canal', 'stream', 'stream_intermittent', 'ditch', 'drain');
 
 CREATE OR REPLACE VIEW waterway_layer AS (
     SELECT osm_id, timestamp, geometry FROM waterway_z8toz12
--- src/import-sql/layers/waterway_label.sql
+++ src/import-sql/layers/waterway_label.sql
@@ -3,13 +3,20 @@ CREATE OR REPLACE VIEW waterway_label_z8toz12 AS
     FROM osm_water_linestring
     WHERE type IN ('river', 'canal');
 
-CREATE OR REPLACE VIEW waterway_label_z13toz14 AS
+CREATE OR REPLACE VIEW waterway_label_z13 AS
     SELECT *
     FROM osm_water_linestring
     WHERE type IN ('river', 'canal', 'stream', 'stream_intermittent');
 
+CREATE OR REPLACE VIEW waterway_label_z14 AS
+    SELECT *
+    FROM osm_water_linestring
+    WHERE type IN ('river', 'canal', 'stream', 'stream_intermittent', 'ditch', 'drain');
+
 CREATE OR REPLACE VIEW waterway_label_layer AS (
     SELECT osm_id, timestamp, geometry FROM waterway_label_z8toz12
     UNION
-    SELECT osm_id, timestamp, geometry FROM waterway_label_z13toz14
+    SELECT osm_id, timestamp, geometry FROM waterway_label_z13
+    UNION
+    SELECT osm_id, timestamp, geometry FROM waterway_label_z14
 );
