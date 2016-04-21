27bede1a0a8a80fd109072a5dcd2c9224e540fbf
Lukas Martinelli <me@lukasmartinelli.ch>
2016-04-21 10:47:10 +0200

    Show drains and ditches in layer waterway

--- src/import-sql/layers/waterway.sql
+++ src/import-sql/layers/waterway.sql
@@ -3,15 +3,22 @@ CREATE OR REPLACE VIEW waterway_z8toz12 AS
     FROM osm_water_linestring
     WHERE type IN ('river', 'canal');
 
-CREATE OR REPLACE VIEW waterway_z13toz14 AS
+CREATE OR REPLACE VIEW waterway_z13 AS
     SELECT *
     FROM osm_water_linestring
     WHERE type IN ('river', 'canal', 'stream', 'stream_intermittent');
 
+CREATE OR REPLACE VIEW waterway_z14 AS
+    SELECT *
+    FROM osm_water_linestring
+    WHERE type IN ('river', 'canal', 'stream', 'stream_intermittent', 'ditch', 'drain');
+
 CREATE OR REPLACE VIEW waterway_layer AS (
     SELECT osm_id, timestamp, geometry FROM waterway_z8toz12
     UNION
-    SELECT osm_id, timestamp, geometry FROM waterway_z13toz14
+    SELECT osm_id, timestamp, geometry FROM waterway_z13
+    UNION
+    SELECT osm_id, timestamp, geometry FROM waterway_z14
 );
 
 CREATE OR REPLACE FUNCTION waterway_changed_tiles(ts timestamp)
