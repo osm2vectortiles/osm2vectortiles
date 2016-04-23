a0b489f19774ccf6b2cb972bdf3e4f406b9357ee
Manuel Roth <manuelroth@hotmail.ch>
2016-04-23 19:49:57 +0200

    Fix type => Multi not Muli :smile

--- src/import-sql/functions.sql
+++ src/import-sql/functions.sql
@@ -80,7 +80,7 @@ CREATE OR REPLACE FUNCTION osm_id_geometry(osm_id BIGINT, geom geometry) RETURNS
 BEGIN RETURN CASE
         WHEN ST_GeometryType(geom) IN ('ST_LineString', 'ST_MultiLineString') THEN osm_id_linestring(osm_id)
         WHEN ST_GeometryType(geom) IN ('ST_Point', 'ST_MultiPoint') THEN osm_id_point(osm_id)
-        WHEN ST_GeometryType(geom) IN ('ST_Polygon', 'ST_MuliPolygon') THEN osm_id_polygon(osm_id)
+        WHEN ST_GeometryType(geom) IN ('ST_Polygon', 'ST_MultiPolygon') THEN osm_id_polygon(osm_id)
         ELSE osm_id_point(osm_id)
       END;
 END;
