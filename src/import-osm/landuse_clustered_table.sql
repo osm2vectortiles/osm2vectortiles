7b8639369b8259b6553d7c2c9e311a0a804c2929
Manuel Roth <manuelroth@hotmail.ch>
2016-04-23 14:54:51 +0200

    Add forest to clustered landuse

--- src/import-osm/landuse_clustered_table.sql
+++ src/import-osm/landuse_clustered_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS osm_landuse_clustered CASCADE;
 CREATE TABLE osm_landuse_clustered AS
 SELECT ST_CollectionExtract(unnest(ST_ClusterWithin(geometry, 5000)),3) AS geometry                    
 FROM osm_landuse_polygon_gen0                                                                         
-WHERE type IN ('wood');
+WHERE type IN ('wood', 'forest');
 
 CREATE INDEX ON osm_landuse_clustered USING gist (geometry);
 CREATE INDEX ON osm_landuse_clustered
