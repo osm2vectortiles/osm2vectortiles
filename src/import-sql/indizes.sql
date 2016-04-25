818393072c9e5ccf92edf652aeefb1d00eaae770
Lukas Martinelli <me@lukasmartinelli.ch>
2016-04-25 19:44:08 +0200

    Fix create index command #289

--- src/import-sql/indizes.sql
+++ src/import-sql/indizes.sql
@@ -1,1 +1,2 @@
-CREATE INDEX IF NOT EXISTS ON osm_road_geometry(road_class(type, service, access));
+DROP INDEX IF EXISTS osm_road_geometry_class;
+CREATE INDEX osm_road_geometry_class ON osm_road_geometry(road_class(type, service, access));
