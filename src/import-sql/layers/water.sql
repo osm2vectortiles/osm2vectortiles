d52b37e1f544028492e72e3196995554d83570b7
Manuel Roth <manuelroth@hotmail.ch>
2016-04-28 18:02:21 +0200

    Use renamed osm_ocean_polygon tables

--- src/import-sql/layers/water.sql
+++ src/import-sql/layers/water.sql
@@ -20,37 +20,37 @@ CREATE OR REPLACE VIEW water_z2toz3 AS
     FROM ne_50m_lakes;
 
 CREATE OR REPLACE VIEW water_z4 AS
-    SELECT 0 AS osm_id, way AS geometry
-    FROM osm_ocean_polygons_gen0
+    SELECT 0 AS osm_id, geometry
+    FROM osm_ocean_polygon_gen0
     UNION ALL
     SELECT 0 AS osm_id, geom AS geometry
     FROM ne_10m_lakes;
 
 CREATE OR REPLACE VIEW water_z5toz7 AS
-    SELECT 0 AS osm_id, way AS geometry
-    FROM osm_ocean_polygons_gen0
+    SELECT 0 AS osm_id, geometry
+    FROM osm_ocean_polygon_gen0
     UNION ALL
     SELECT osm_id, geometry
     FROM osm_water_polygon_gen1;
 
 CREATE OR REPLACE VIEW water_z8toz10 AS
-    SELECT 0 AS osm_id, way AS geometry
-    FROM osm_ocean_polygons
+    SELECT 0 AS osm_id, geometry
+    FROM osm_ocean_polygon
     UNION ALL
     SELECT osm_id, geometry
     FROM osm_water_polygon_gen1;
 
 CREATE OR REPLACE VIEW water_z11toz12 AS
-    SELECT 0 AS osm_id, way AS geometry, 0 AS area
-    FROM osm_ocean_polygons
+    SELECT 0 AS osm_id, geometry, 0 AS area
+    FROM osm_ocean_polygon
     UNION ALL
     SELECT osm_id, geometry, area
     FROM osm_water_polygon
     WHERE area >= 15000;
 
 CREATE OR REPLACE VIEW water_z13toz14 AS
-    SELECT 0 AS osm_id, way AS geometry
-    FROM osm_ocean_polygons
+    SELECT 0 AS osm_id, geometry
+    FROM osm_ocean_polygon
     UNION ALL
     SELECT osm_id, geometry
     FROM osm_water_polygon;
