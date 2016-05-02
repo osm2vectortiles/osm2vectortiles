9649e62081b7ad71cf34cb56f6e0c6f4a56e6dcc
Manuel Roth <manuelroth@hotmail.ch>
2016-05-02 18:06:15 +0200

    Show landuse=residential from z8toz14

--- src/import-sql/layers/landuse.sql
+++ src/import-sql/layers/landuse.sql
@@ -1,30 +1,35 @@
-CREATE OR REPLACE VIEW landuse_z5toz8 AS
+CREATE OR REPLACE VIEW landuse_z5toz7 AS
     SELECT osm_id, geometry, type
     FROM osm_landuse_polygon_gen0
     WHERE type IN ('wood', 'forest');
 
+CREATE OR REPLACE VIEW landuse_z8 AS
+    SELECT osm_id, geometry, type
+    FROM osm_landuse_polygon_gen0
+    WHERE type IN ('wood', 'forest', 'residential');
+
 CREATE OR REPLACE VIEW landuse_z9 AS
     SELECT *
     FROM osm_landuse_polygon_gen0
-    WHERE landuse_class(type) IN ('wood', 'grass', 'cemetery', 'park', 'school')
+    WHERE landuse_class(type) IN ('wood', 'forest', 'residential', 'grass', 'cemetery', 'park', 'school')
       AND st_area(geometry) > 500000;
 
 CREATE OR REPLACE VIEW landuse_z10 AS
     SELECT *
     FROM osm_landuse_polygon_gen0
-    WHERE landuse_class(type) IN ('wood', 'grass', 'cemetery', 'park', 'school')
+    WHERE landuse_class(type) IN ('wood', 'forest', 'residential', 'grass', 'cemetery', 'park', 'school')
       AND st_area(geometry) > 99000;
 
 CREATE OR REPLACE VIEW landuse_z11 AS
     SELECT *
     FROM osm_landuse_polygon_gen1
-    WHERE landuse_class(type) IN ('wood', 'grass', 'cemetery', 'park', 'school', 'hospital')
+    WHERE landuse_class(type) IN ('wood', 'forest', 'residential', 'grass', 'cemetery', 'park', 'school', 'hospital')
       AND st_area(geometry) > 50000;
 
 CREATE OR REPLACE VIEW landuse_z12 AS
     SELECT *
     FROM osm_landuse_polygon
-    WHERE landuse_class(type) IN ('wood', 'grass', 'cemetery', 'park', 'school', 'hospital')
+    WHERE landuse_class(type) IN ('wood', 'forest', 'residential', 'grass', 'cemetery', 'park', 'school', 'hospital')
       AND st_area(geometry) > 10000;
 
 CREATE OR REPLACE VIEW landuse_z13toz14 AS
@@ -33,7 +38,9 @@ CREATE OR REPLACE VIEW landuse_z13toz14 AS
     WHERE type NOT IN ('wetland', 'marsh', 'swamp', 'bog', 'mud', 'tidalflat', 'national_park', 'nature_reserve', 'protected_area');
 
 CREATE OR REPLACE VIEW landuse_layer AS (
-    SELECT osm_id FROM landuse_z5toz8
+    SELECT osm_id FROM landuse_z5toz7
+    UNION
+    SELECT osm_id FROM landuse_z8
     UNION
     SELECT osm_id FROM landuse_z9
     UNION
