aad6cc0d391a476defdbbc90f011dc7e3922fc28
Lukas Martinelli <me@lukasmartinelli.ch>
2016-05-02 19:09:30 +0200

    Hide railway station labels on z12

--- src/import-sql/layers/rail_station_label.sql
+++ src/import-sql/layers/rail_station_label.sql
@@ -3,14 +3,14 @@ CREATE OR REPLACE VIEW rail_station_label_z14 AS (
     FROM osm_rail_station_point
 );
 
-CREATE OR REPLACE VIEW rail_station_label_z12toz13 AS (
+CREATE OR REPLACE VIEW rail_station_label_z13 AS (
     SELECT *
     FROM osm_rail_station_point
     WHERE rail_station_class(type) = 'rail'
 );
 
 CREATE OR REPLACE VIEW rail_station_label_layer AS (
-    SELECT osm_id FROM rail_station_label_z12toz13
+    SELECT osm_id FROM rail_station_label_z13
     UNION ALL
     SELECT osm_id FROM rail_station_label_z14
 );
