946b9dd31c4a7745709bfb19c6b37b21be43bc0c
Lukas Martinelli <me@lukasmartinelli.ch>
2016-05-02 15:06:57 +0200

    Add support for lower zoom extracts and upload

--- src/create-extracts/create-extracts.sh
+++ src/create-extracts/create-extracts.sh
@@ -3,50 +3,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-readonly WORLD_MBTILES=${WORLD_MBTILES:-"world.mbtiles"}
-readonly PATCH_MBTILES=${PATCH_MBTILES:-"world_z0-z5.mbtiles"}
 readonly CITIES_TSV=${CITIES_TSV:-"city_extracts.tsv"}
 readonly COUNTRIES_TSV=${COUNTRIES_TSV:-"country_extracts.tsv"}
+readonly WORLD_MBTILES=${WORLD_MBTILES:-"world.mbtiles"}
 readonly EXTRACT_DIR=$(dirname "$WORLD_MBTILES")
-readonly VERSION=${VERSION:-1}
-readonly GLOBAL_BBOX="-180, -85.0511, 180, 85.0511"
-readonly S3_CONFIG_FILE=${S3_CONFIG_FILE:-"$HOME/.s3cfg"}
-readonly S3_BUCKET_NAME=${S3_BUCKET_NAME:-"osm2vectortiles-downloads"}
-readonly S3_PREFIX=${S3_PREFIX:-"v1.0/extracts/"}
+readonly PATCH_SRC="$EXTRACT_DIR/world_z0-z5.mbtiles"
 
-function upload_extract() {
-    local mbtiles_extract="$1"
-    s3cmd --config="$S3_CONFIG_FILE" \
-          --access_key="$S3_ACCESS_KEY" \
-          --secret_key="$S3_SECRET_KEY" \
-        put "$mbtiles_extract" "s3://$S3_BUCKET_NAME/$S3_PREFIX" \
-          --acl-public \
-          --multipart-chunk-size-mb=50
-}
-
-function create_lower_zoomlevel_extract() {
-    local extract_file="$EXTRACT_DIR/$1"
-    local min_zoom="$2"
-    local max_zoom="$3"
-
-    local center_zoom="1"
-    local center_longitude="-94.1629"
-    local center_latitude="34.5133"
-    local center="$center_longitude,$center_latitude,$center_zoom"
-
-    echo "Create extract $extract_file"
-    tilelive-copy \
-        --minzoom="$min_zoom" \
-        --maxzoom="$max_zoom" \
-        --bounds="$GLOBAL_BBOX" \
-        "$WORLD_MBTILES" "$extract_file"
-
-    echo "Update metadata $extract_file"
-    update_metadata "$extract_file" "$GLOBAL_BBOX" "$center" "$min_zoom" "$max_zoom"
-
-    echo "Uploading $extract_file"
-    upload_extract "$extract_file"
-}
 
 function main() {
     if [ ! -f "$WORLD_MBTILES" ]; then
@@ -54,12 +16,22 @@ function main() {
         exit 10
     fi
 
+    local upload_flag='--upload'
     if [ -z "${S3_ACCESS_KEY}" ]; then
+        upload_flag=''
         echo 'Skip upload since no S3_ACCESS_KEY was found.'
     fi
 
-    python create_extracts.py "$WORLD_MBTILES" "$CITIES_TSV" --target-dir="$EXTRACT_DIR"
-    python create_extracts.py "$WORLD_MBTILES" "$COUNTRIES_TSV" --target-dir="$EXTRACT_DIR"
+    # Generate patch sources first
+    python create_extracts.py zoom-level "$WORLD_MBTILES" \
+        --max-zoom=5 --target-dir="$EXTRACT_DIR" $upload_flag
+    python create_extracts.py zoom-level "$WORLD_MBTILES" \
+        --max-zoom=8 --target-dir="$EXTRACT_DIR" $upload_flag
+
+    python create_extracts.py bbox "$WORLD_MBTILES" "$CITIES_TSV" \
+        --patch-from="$PATCH_SRC" --target-dir="$EXTRACT_DIR" $upload_flag
+    python create_extracts.py bbox "$WORLD_MBTILES" "$COUNTRIES_TSV" \
+        --patch-from="$PATCH_SRC" --target-dir="$EXTRACT_DIR" $upload_flag
 }
 
 main
--- src/create-extracts/create_extracts.py
+++ src/create-extracts/create_extracts.py
@@ -2,14 +2,18 @@
 """Generate jobs for rendering tiles in pyramid and list format in JSON format
 
 Usage:
-  create_extracts.py <source_file> <tsv_file> [--concurrency=<concurrency>] [--target-dir=<target-dir>]
+  create_extracts.py bbox <source_file> <tsv_file> [--patch-from=<patch-src>]  [--upload] [--concurrency=<concurrency>] [--target-dir=<target-dir>]
+  create_extracts.py zoom-level <source_file> --max-zoom=<max-zoom> [--upload] [--target-dir=<target-dir>]
   create_extracts.py (-h | --help)
   create_extracts.py --version
 
 Options:
   -h --help                     Show this screen.
   --version                     Show version.
+  --upload                      Upload extracts to S3.
+  --patch-from=<patch-src>      Patch MBTiles file with other MBTiles src.
   --concurrency=<concurrency>   Number of copy processes to use [default: 4].
+  --max-zoom=<max-zoom>         Max zoom level of low zoom level extract.
   --target-dir=<target-dir>     Target directory to put extracts in [default: ./]
 """
 
@@ -28,7 +32,8 @@ VERSION = '1.5'
 
 class Extract(object):
 
-    def __init__(self, extract, country, city, top, left, bottom, right):
+    def __init__(self, extract, country, city, top, left, bottom, right,
+                 min_zoom=0, max_zoom=14, center_zoom=10):
         self.extract = extract
         self.country = country
         self.city = city
@@ -38,9 +43,9 @@ class Extract(object):
         self.max_lon = right
         self.max_lat = top
 
-        self.min_zoom = 0
-        self.max_zoom = 14
-        self.center_zoom = 10
+        self.min_zoom = min_zoom
+        self.max_zoom = max_zoom
+        self.center_zoom = center_zoom
 
     def bounds(self):
         return '{},{},{},{}'.format(self.min_lon, self.min_lat,
@@ -101,14 +106,14 @@ def update_metadata(mbtiles_file, metadata):
 
 def patch_mbtiles(source_file, target_file):
     conn = mbutil.mbtiles_connect(target_file)
-    conn.execute(
+    conn.executescript(
         """
         PRAGMA journal_mode=PERSIST;
         PRAGMA page_size=80000;
         PRAGMA synchronous=OFF;
         ATTACH DATABASE '{}' AS source;
         REPLACE INTO map SELECT * FROM source.map;
-        REPLACE INTO images SELECT * FROM source.images;"
+        REPLACE INTO images SELECT * FROM source.images;
         """.format(source_file)
     )
 
@@ -128,23 +133,75 @@ def parse_extracts(tsv_file):
             )
 
 
+def upload_mbtiles(mbtiles_file):
+    """
+    Use s3cmd to upload file. This is easier than using Boto directly.
+    The upload command is configured via environment variables and not
+    the extract utility command line flags.
+    """
+    config_file = os.getenv('S3_CONFIG_FILE',
+                            os.path.join(os.environ['HOME'], '.s3cfg'))
+    access_key = os.environ['S3_ACCESS_KEY']
+    secret_key = os.environ['S3_SECRET_KEY']
+    bucket_name = os.getenv('S3_BUCKET_NAME', 'osm2vectortiles-downloads')
+    prefix = os.getenv('S3_PREFIX', '{}/{}/'.format(VERSION, 'extracts'))
+
+    subprocess.check_call([
+        's3cmd',
+        '--config={}'.format(config_file),
+        '--access_key={}'.format(access_key),
+        '--secret_key={}'.format(secret_key),
+        'put', mbtiles_file,
+        's3://{}/{}'.format(bucket_name, prefix),
+        '--acl-public',
+        '--multipart-chunk-size-mb=50'
+    ])
+
+
 if __name__ == '__main__':
     args = docopt(__doc__, version='0.1')
 
-    process_count = int(args['--concurrency'])
     target_dir = args['--target-dir']
+    upload = args['--upload']
     source_file = args['<source_file>']
-    extracts = list(parse_extracts(args['<tsv_file>']))
 
     def process_extract(extract):
-        extract_file = os.path.join(target_dir, extract.extract + '.mbtiles')
+        patch_src = args['--patch-from']
 
+        extract_file = os.path.join(target_dir, extract.extract + '.mbtiles')
         print('Create extract {}'.format(extract_file))
         create_extract(extract, source_file, extract_file)
         print('Update metadata {}'.format(extract_file))
         update_metadata(extract_file, extract.metadata(extract_file))
 
-    pool = ProcessPool(process_count)
-    pool.map(process_extract, extracts)
-    pool.close() 
-    pool.join() 
+        if patch_src:
+            print('Patch from {}'.format(patch_src))
+            patch_mbtiles(patch_src, extract_file)
+
+        if upload:
+            print('Upload file {}'.format(extract_file))
+            upload_mbtiles(extract_file)
+
+
+    if args['bbox']:
+        process_count = int(args['--concurrency'])
+        extracts = list(parse_extracts(args['<tsv_file>']))
+        pool = ProcessPool(process_count)
+        pool.map(process_extract, extracts)
+        pool.close()
+        pool.join()
+
+    if args['zoom-level']:
+        max_zoom_level = int(args['--max-zoom'])
+        extract = Extract('world_z0-z{}'.format(max_zoom_level),
+                          country=None,
+                          city=None,
+                          left=-180,
+                          right=180,
+                          top=85.0511,
+                          bottom=-85.0511,
+                          center_zoom=2,
+                          min_zoom=0,
+                          max_zoom=max_zoom_level
+                         )
+        process_extract(extract)
