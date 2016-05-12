192cecb70445b30e8f3fbc7fc6b77fdd491ff805
Manuel Roth <manuelroth@hotmail.ch>
2016-05-12 16:52:50 +0200

    Revamp downloads section, use countries, cities tsv-file as datasource

--- js/index.js
+++ js/index.js
@@ -40,22 +40,94 @@ function init() {
     };
   }
 
-  /* Search field in downloads section */
-  var searchField = document.querySelector('#searchField');
-  if (searchField) {
-    searchField.onkeyup = function() {
-      var searchText = searchField.value.toLowerCase();
-      var countries = document.querySelector('#searchField').parentNode.nextElementSibling.children;
-      for(var i = 0; i <= countries.length - 1; ++i) {
+  var search_countries = document.querySelector('#search_countries');
+  if (search_countries) {
+    search_countries.onkeyup = function() {
+      var searchText = search_countries.value.toLowerCase();
+      var countries = document.querySelector('#country').children;
+      for(var i = 1; i <= countries.length - 1; ++i) {
         var element = countries[i];
-        var countryName = element.textContent.toLowerCase();
+        var countryName = element.children[0].children[0].textContent.toLowerCase();
         if(!( countryName.indexOf(searchText) != -1 )) {
           element.style.display = 'none';
         } else {
-          element.style.display = 'list-item';
+          element.style.display = 'block';
         }
       }
     }
   }
+
+  var country = document.querySelector('#country');
+  if(country) {
+    var template = '<div class="col12 download-item" onclick="{{{url}}}"><div class="col4 download-title">{{ title }}</div><div class="col2">{{ size }}</div></div>';
+    Mustache.parse(template);
+    d3.tsv("https://raw.githubusercontent.com/osm2vectortiles/osm2vectortiles/master/src/create-extracts/country_extracts.tsv", function(error, data) {
+      if (error) throw error;
+      data.forEach(function(d) {
+        var data = {
+          "url": "location.href='https://osm2vectortiles-downloads.os.zhdk.cloud.switch.ch/v1.0/extracts/" + d.extract + ".mbtiles'",
+          "title": d.country,
+          "size": "20 MB"
+        };
+        var element = document.createElement("div");
+        element.innerHTML = Mustache.render(template, data);
+        document.querySelector("#country").appendChild(element);
+      });
+    });
+  }
+
+  var search_cities = document.querySelector('#search_cities');
+  if (search_cities) {
+    search_cities.onkeyup = function() {
+      var searchText = search_cities.value.toLowerCase();
+      var cities = document.querySelector('#city').children;
+      for(var i = 1; i <= cities.length - 1; ++i) {
+        var element = cities[i];
+        var countryName = element.children[0].children[0].textContent.toLowerCase();
+        if(!( countryName.indexOf(searchText) != -1 )) {
+          element.style.display = 'none';
+        } else {
+          element.style.display = 'block';
+        }
+      }
+    }
+  }
+
+  var city = document.querySelector('#city');
+  if(city) {
+    var template = '<div class="col12 download-item" onclick="{{{url}}}"><div class="col4 download-title">{{ title }}</div><div class="col2">{{ size }}</div></div>';
+    Mustache.parse(template);
+    d3.tsv("https://raw.githubusercontent.com/osm2vectortiles/osm2vectortiles/master/src/create-extracts/city_extracts.tsv", function(error, data) {
+      if (error) throw error;
+      data.forEach(function(d) {
+        var data = {
+          "url": "location.href='https://osm2vectortiles-downloads.os.zhdk.cloud.switch.ch/v1.0/extracts/" + d.extract + ".mbtiles'",
+          "title": d.country + ", " + d.city,
+          "size": "20 MB"
+        };
+        var element = document.createElement("div");
+        element.innerHTML = Mustache.render(template, data);
+        document.querySelector("#city").appendChild(element);
+      });
+    });
+  }
 }
 window.onload = init;
+
+function showPlanet() {
+  document.querySelector("#city").style.display = "none";
+  document.querySelector("#country").style.display = "none";
+  document.querySelector("#planet").style.display = "block";
+}
+
+function showCountry() {
+  document.querySelector("#city").style.display = "none";
+  document.querySelector("#country").style.display = "block";
+  document.querySelector("#planet").style.display = "none";
+}
+
+function showCity() {
+  document.querySelector("#city").style.display = "block";
+  document.querySelector("#country").style.display = "none";
+  document.querySelector("#planet").style.display = "none";
+}
