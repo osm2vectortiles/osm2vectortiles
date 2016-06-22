917ba3a3903ecbf60d4c71e31ad5d040a6003b81
lukasmartinelli <me@lukasmartinelli.ch>
2016-06-22 21:10:21 +0200

    Add job to render planet from z0 to z7

--- src/generate-jobs/generate_world_jobs.sh
+++ src/generate-jobs/generate_world_jobs.sh
@@ -14,7 +14,8 @@ function generate_world_jobs() {
     local jobs_file="$EXPORT_DATA_DIR/world_jobs.txt"
     local jobs_queue="jobs"
 
-    python generate_jobs.py pyramid "$TILE_X" "$TILE_Y" "$TILE_Z" --job-zoom="$JOB_ZOOM" > $jobs_file
+    python generate_jobs.py pyramid "0" "0" "0" --job-zoom="0" --max-zoom="$((JOB_ZOOM-1))" > $jobs_file
+    python generate_jobs.py pyramid "$TILE_X" "$TILE_Y" "$TILE_Z" --job-zoom="$JOB_ZOOM" >> $jobs_file
     pipecat publish --amqpuri="$AMQP_URI" "$jobs_queue" < $jobs_file
 }
 
