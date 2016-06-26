4438f3842f9aea819eca0dc5e3e7b92e217eab30
lukasmartinelli <me@lukasmartinelli.ch>
2016-06-26 23:08:51 +0200

    Add initial mapping for motorway junction #359

---
+++ src/import-sql/layers/motorway_junction.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW motorway_junction_z11 AS
+    SELECT id AS osm_id, type, ref, name, geometry
+    FROM osm_motorway_junction_point
+    WHERE ref <> '' AND type <> 'trunk';
+
+CREATE OR REPLACE VIEW motorway_junction_z12_toz14 AS
+    SELECT id AS osm_id, type, ref, name, geometry
+    FROM osm_motorway_junction_point
+    WHERE ref <> '';
