a76b5e9331a48153fe0bd19b4b2eabb591269246
lukasmartinelli <me@lukasmartinelli.ch>
2016-06-28 08:09:37 +0200

    Use pseudoarea in landuse

--- src/import-sql/layers/landuse.sql
+++ src/import-sql/layers/landuse.sql
@@ -7,31 +7,31 @@ CREATE OR REPLACE VIEW landuse_z7toz8 AS
     SELECT id AS osm_id, geometry, type
     FROM osm_landuse_polygon_gen0
     WHERE landuse_class(type) IN ('wood', 'residential')
-      AND st_area(geometry) > 1000000;
+      AND area > 1000000;
 
 CREATE OR REPLACE VIEW landuse_z9 AS
     SELECT id AS osm_id, geometry, type
     FROM osm_landuse_polygon_gen0
     WHERE landuse_class(type) IN ('wood', 'residential', 'grass', 'cemetery', 'park', 'school')
-      AND st_area(geometry) > 500000;
+      AND area > 500000;
 
 CREATE OR REPLACE VIEW landuse_z10 AS
     SELECT id AS osm_id, geometry, type
     FROM osm_landuse_polygon_gen0
     WHERE landuse_class(type) IN ('wood', 'residential', 'commercial', 'retail', 'railway', 'industrial', 'grass', 'cemetery', 'park', 'school')
-      AND st_area(geometry) > 99000;
+      AND area > 99000;
 
 CREATE OR REPLACE VIEW landuse_z11 AS
     SELECT id AS osm_id, geometry, type
     FROM osm_landuse_polygon_gen1
     WHERE landuse_class(type) IN ('wood', 'residential','commercial', 'retail', 'railway', 'industrial', 'military', 'grass', 'cemetery', 'park', 'school', 'hospital')
-      AND st_area(geometry) > 50000;
+      AND area > 50000;
 
 CREATE OR REPLACE VIEW landuse_z12 AS
     SELECT id AS osm_id, geometry, type
     FROM osm_landuse_polygon
     WHERE landuse_class(type) IN ('wood', 'residential', 'grass','retail', 'railway', 'industrial', 'military', 'cemetery', 'park', 'school', 'hospital')
-      AND st_area(geometry) > 10000;
+      AND area > 10000;
 
 CREATE OR REPLACE VIEW landuse_z13toz14 AS
     SELECT id AS osm_id, geometry, type
