200f0688547c0a1a4bbe5a5c3f02c04f067e5742
stirringhalo <stirringhalo@users.noreply.github.com>
2016-07-31 11:54:16 -0400

    Import-osm2pgsql now working

--- src/import-osm2pgsql/import.sh
+++ src/import-osm2pgsql/import.sh
@@ -13,11 +13,19 @@ readonly PG_CONNECT="postgis://$OSM_USER:$OSM_PASSWORD@$DB_HOST/$OSM_DB"
 
 function import_all() {
     local pbf_file="$1"
-    osm2pgsql -d "$OSM_DB" -U "$OSM_USER" -W "$OSM_PASSWORD" -H "$DB_HOST" "$pbf_file" --create --slim --cache 1000 --number-process 2 --hstore --multi-geometry --style openstreetmap-carto.style
 
-    ./get-shapefiles.sh
+    echo $pbf_file
+    export pgpassword="$OSM_PASSWORD"
+    osm2pgsql --create --slim --cache 1000 --number-processes 2 --hstore  --style "/openstreetmap-carto-vector-tiles/openstreetmap-carto.style" --multi-geometry --database="$OSM_DB" --username="$OSM_USER" --host="$DB_HOST" "$pbf_file"
+    echo "Done import"
 
-    psql -d gis -f indexes.sql
+    cd /openstreetmap-carto-vector-tiles/
 
-    make reindexshapefiles postgresql-indexes
+    psql -d "$OSM_DB" -h "$DB_HOST" -U "$OSM_USER" -f /openstreetmap-carto-vector-tiles/indexes.sql 
+    PGOPTIONS='--client-min-messages=fatal' psql -d "$OSM_DB" -h "$DB_HOST" -U "$OSM_USER" -f add-indexes.sql || true
+    echo "Done indexing"
+
+    bash /openstreetmap-carto-vector-tiles/get-shapefiles.sh # Might need to put this into db
+    make install-node-modules
+    make reindexshapefiles #Might need to put this into db
 }
