455db3830d550a4243f3634e1885cf92f6b2cd8d
lukasmartinelli <me@lukasmartinelli.ch>
2016-08-27 23:16:57 +0200

    Rename table and drop area column

--- src/import-osm/water_point_table.sql
+++ src/import-osm/water_point_table.sql
@@ -11,8 +11,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
 
-DROP TABLE IF EXISTS osm_water_point CASCADE;
-CREATE TABLE osm_water_point AS
+DROP TABLE IF EXISTS osm_water_label CASCADE;
+CREATE TABLE osm_water_label AS
 SELECT id,
        COALESCE(ll.wkb_geometry, topoint(wp.geometry)) AS geometry,
        timestamp,
@@ -22,6 +22,6 @@ SELECT id,
 FROM osm_water_polygon AS wp
 LEFT JOIN custom_lakeline ll ON imposm_id_to_osm_id(wp.id, true) = ll.osm_id;
 
-CREATE INDEX ON osm_water_point USING gist (geometry);
-CREATE INDEX ON osm_water_point
+CREATE INDEX ON osm_water_label USING gist (geometry);
+CREATE INDEX ON osm_water_label
 USING btree (st_geohash(st_transform(st_setsrid(box2d(geometry)::geometry, 3857), 4326)));
