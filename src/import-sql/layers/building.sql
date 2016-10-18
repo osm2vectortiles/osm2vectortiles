582699f17e2e20885003365fd5d9d2beb5669961
stirringhalo <stirringhalo@users.noreply.github.com>
2016-10-18 18:59:38 -0400

    Not fully working yet, weirdness in that buildings render all the same height

--- src/import-sql/layers/building.sql
+++ src/import-sql/layers/building.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW building_z13 AS
-    SELECT id AS osm_id, underground, geometry
+    SELECT id AS osm_id, underground, geometry, height, levels, min_height, min_level
     FROM osm_building_polygon_gen0;
 
 CREATE OR REPLACE VIEW building_z14 AS
-    SELECT id AS osm_id, underground, geometry
+    SELECT id AS osm_id, underground, geometry, height, levels, min_height, min_level
     FROM osm_building_polygon;
 
 CREATE OR REPLACE VIEW building_layer AS (
@@ -12,6 +12,29 @@ CREATE OR REPLACE VIEW building_layer AS (
     SELECT osm_id FROM building_z14
 );
 
+
+CREATE OR REPLACE FUNCTION default_building_height(levels INTEGER) RETURNS INTEGER
+AS $$
+BEGIN
+    IF levels IS NULL THEN
+        RETURN 1;
+    ELSE
+        RETURN levels;
+    END IF;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION default_building_base_height(min_level INTEGER) RETURNS INTEGER
+AS $$
+BEGIN
+    IF min_level IS NULL THEN
+        RETURN 0;
+    ELSE
+        RETURN min_level;
+    END IF;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
 CREATE OR REPLACE FUNCTION building_is_underground(level INTEGER) RETURNS VARCHAR
 AS $$
 BEGIN
@@ -19,6 +42,28 @@ BEGIN
         RETURN 'true';
     ELSE
         RETURN 'false';
+    END IF;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION building_height(height INTEGER, levels INTEGER) RETURNS INTEGER
+AS $$
+BEGIN
+    IF height IS NULL THEN
+        RETURN 3*default_building_height(levels);
+    ELSE
+        RETURN height;
+    END IF;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION building_base_height(min_height INTEGER, min_level INTEGER) RETURNS INTEGER
+AS $$
+BEGIN
+    IF min_height IS NULL THEN
+        RETURN 3*default_building_base_height(min_level);
+    ELSE
+        RETURN min_height;
     END IF;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
