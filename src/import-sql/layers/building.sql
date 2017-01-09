40a4073326a53a9e71c398b5342767950db15008
chrissng <chris@data.gov.sg>
2017-01-09 09:41:14 +0800

    more building heights
    - infer building heights from building:levels property
    - default building height of 6m

--- src/import-sql/layers/building.sql
+++ src/import-sql/layers/building.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW building_z13 AS
-    SELECT id AS osm_id, underground, height, min_height, geometry
+    SELECT id AS osm_id, underground, height, min_height, building_levels, geometry
     FROM osm_building_polygon_gen0;
 
 CREATE OR REPLACE VIEW building_z14 AS
-    SELECT id AS osm_id, underground, height, min_height, geometry
+    SELECT id AS osm_id, underground, height, min_height, building_levels, geometry
     FROM osm_building_polygon;
 
 CREATE OR REPLACE VIEW building_layer AS (
@@ -11,6 +11,19 @@ CREATE OR REPLACE VIEW building_layer AS (
     UNION
     SELECT osm_id FROM building_z14
 );
+
+CREATE OR REPLACE FUNCTION infer_building_height(height INTEGER, building_levels INTEGER) RETURNS VARCHAR
+AS $$
+BEGIN
+    IF height IS NOT NULL THEN
+        RETURN height;
+    ELSIF building_levels IS NOT NULL THEN
+        RETURN (building_levels + 1) * 3;
+    ELSE
+        RETURN 6;
+    END IF;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION building_is_underground(level INTEGER) RETURNS VARCHAR
 AS $$
