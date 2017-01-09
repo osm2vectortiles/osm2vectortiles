2f1b1992e1c99c589c340ef4adadda7e0a20175e
Chris <chris@data.gov.sg>
2017-01-09 22:03:46 +0800

    add min_level and infer height of building's base

--- src/import-sql/layers/building.sql
+++ src/import-sql/layers/building.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW building_z13 AS
-    SELECT id AS osm_id, underground, height, min_height, building_levels, geometry
+    SELECT id AS osm_id, underground, height, min_height, levels, min_level, geometry
     FROM osm_building_polygon_gen0;
 
 CREATE OR REPLACE VIEW building_z14 AS
-    SELECT id AS osm_id, underground, height, min_height, building_levels, geometry
+    SELECT id AS osm_id, underground, height, min_height, levels, min_level, geometry
     FROM osm_building_polygon;
 
 CREATE OR REPLACE VIEW building_layer AS (
@@ -12,15 +12,28 @@ CREATE OR REPLACE VIEW building_layer AS (
     SELECT osm_id FROM building_z14
 );
 
-CREATE OR REPLACE FUNCTION infer_building_height(height INTEGER, building_levels INTEGER) RETURNS VARCHAR
+CREATE OR REPLACE FUNCTION infer_height(height INTEGER, levels INTEGER) RETURNS VARCHAR
 AS $$
 BEGIN
     IF height IS NOT NULL THEN
         RETURN height;
-    ELSIF building_levels IS NOT NULL THEN
-        RETURN (building_levels + 1) * 3;
+    ELSIF levels IS NOT NULL THEN
+        RETURN (levels + 1) * 3;
     ELSE
         RETURN 6;
+    END IF;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION infer_min_height(min_height INTEGER, min_level INTEGER) RETURNS VARCHAR
+AS $$
+BEGIN
+    IF min_height IS NOT NULL THEN
+        RETURN min_height;
+    ELSIF min_level IS NOT NULL THEN
+        RETURN min_level * 3;
+    ELSE
+        RETURN 0;
     END IF;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
